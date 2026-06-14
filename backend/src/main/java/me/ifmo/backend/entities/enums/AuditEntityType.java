09c10038f7568071510feb53cb837c9ecd776765
misticslippers <eternal.martyr13579@gmail.com>
2026-06-14 19:09:25 +0300

    add AuditEntityType

---
+++ backend/src/main/java/me/ifmo/backend/entities/enums/AuditEntityType.java
@@ -0,0 +1,21 @@
+package me.ifmo.backend.entities.enums;
+
+public enum AuditEntityType {
+    USER,
+    ROLE,
+    LIBRARY,
+    BRANCH,
+    LIBRARY_RULE,
+    GENRE,
+    AUTHOR,
+    MATERIAL,
+    MATERIAL_COPY,
+    RESERVATION,
+    LOAN,
+    FINE_TARIFF,
+    FINE,
+    PAYMENT_TRANSACTION,
+    NOTIFICATION,
+    USER_BLOCK,
+    USER_WARNING
+}
