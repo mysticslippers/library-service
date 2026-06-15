5640a33d87b24f563d006fc89514494ff3707078
misticslippers <eternal.martyr13579@gmail.com>
2026-06-15 15:53:15 +0300

    add Material

---
+++ backend/src/main/java/me/ifmo/backend/entities/Material.java
@@ -0,0 +1,80 @@
+package me.ifmo.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import me.ifmo.backend.entities.enums.MaterialStatus;
+import me.ifmo.backend.entities.enums.MaterialType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "materials")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "isbn", unique = true, length = 13)
+    private String isbn;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "publication_year")
+    private Integer publicationYear;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(name = "material_type", nullable = false, columnDefinition = "material_type")
+    private MaterialType materialType = MaterialType.BOOK;
+
+    @Builder.Default
+    @Column(name = "language", nullable = false, length = 50)
+    private String language = "ru";
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(name = "status", nullable = false, columnDefinition = "material_status")
+    private MaterialStatus status = MaterialStatus.ACTIVE;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Material material = (Material) object;
+        return id != null && Objects.equals(id, material.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
