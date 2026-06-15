bc170c9e22f23217cf56dcb2719c8d462151088f
misticslippers <eternal.martyr13579@gmail.com>
2026-06-15 16:03:05 +0300

    add PaymentTransaction

---
+++ backend/src/main/java/me/ifmo/backend/entities/PaymentTransaction.java
@@ -0,0 +1,48 @@
+package me.ifmo.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import me.ifmo.backend.entities.enums.PaymentStatus;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment_transactions")
+public class PaymentTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "fine_id", nullable = false)
+    private Fine fine;
+
+    @Column(name = "external_payment_id", unique = true)
+    private String externalPayment;
+
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(name = "status", nullable = false, columnDefinition = "payment_status")
+    private PaymentStatus status = PaymentStatus.CREATED;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
