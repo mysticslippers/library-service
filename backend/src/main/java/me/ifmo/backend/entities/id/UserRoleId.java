efb3591c7eae6c236dd800e4d014942787731a96
misticslippers <eternal.martyr13579@gmail.com>
2026-06-15 16:05:10 +0300

    add UserRoleId

---
+++ backend/src/main/java/me/ifmo/backend/entities/id/UserRoleId.java
@@ -0,0 +1,23 @@
+package me.ifmo.backend.entities.id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleId implements Serializable {
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "role_id")
+    private Long roleId;
+}
