612a13c94d5ee6697efd3a4beee16b2288ef90c0
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 16:50:00 +0300

    add LibraryRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/LibraryRepository.java
@@ -0,0 +1,14 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Library;
+import me.ifmo.backend.entities.enums.LibraryStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LibraryRepository extends JpaRepository<Library, Long> {
+
+    Optional<Library> findByCode(String code);
+}
