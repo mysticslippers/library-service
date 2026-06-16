f7ce75b72b6fe8bf8b85c26fb28f3fb11aaaf618
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:04:59 +0300

    add UserRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.User;
+import me.ifmo.backend.entities.enums.UserStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
