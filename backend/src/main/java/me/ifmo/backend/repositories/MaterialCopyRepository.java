818db14863af03aa57e061ad94ff9c0b60d7a3bb
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 16:55:00 +0300

    add MaterialCopyRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialCopyRepository.java
@@ -0,0 +1,17 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.entities.MaterialCopy;
+import me.ifmo.backend.entities.enums.CopyStatus;
+import me.ifmo.backend.entities.enums.MaterialStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MaterialCopyRepository extends JpaRepository<MaterialCopy, Long> {
+
+    Optional<MaterialCopy> findByInventoryNumber(String inventoryNumber);
+}
