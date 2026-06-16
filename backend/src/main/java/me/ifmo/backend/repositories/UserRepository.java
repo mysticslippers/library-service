db2b5728dd47f5a7ce4a1cf90b218e21811c023e
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:07:09 +0300

    update UserRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserRepository.java
@@ -23,4 +23,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Page<User> findByStatus(UserStatus status, Pageable pageable);
 
     Page<User> findByBranchId(Long branchId, Pageable pageable);
+
+    @Query("""
+           SELECT user FROM User user
+               WHERE (:query IS NULL OR :query = ''
+                   OR lower(user.email) LIKE lower(concat('%', :query, '%')) 
+                   OR user.phone LIKE concat('%', :query, '%') 
+                   OR lower(user.firstName) LIKE lower(concat('%', :query, '%')) 
+                   OR lower(user.lastName) LIKE lower(concat('%', :query, '%'))
+                   OR lower(coalesce(user.middleName, '')) LIKE lower(concat('%', :query, '%')))
+    """)
+    Page<User> search(@Param("query") String query, Pageable pageable);
 }
