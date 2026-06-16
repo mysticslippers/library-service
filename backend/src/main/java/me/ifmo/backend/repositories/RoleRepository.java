2479916ae4af4597cda0de1f7e92a371f0171e2a
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:04:30 +0300

    update RoleRepository

--- backend/src/main/java/me/ifmo/backend/repositories/RoleRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/RoleRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByCode(RoleCode code);
+
+    boolean existsByCode(RoleCode code);
 }
