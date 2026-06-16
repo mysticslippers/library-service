33b90fdcce1e1542fd3eeae63a5e7b11f1685a9d
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:06:14 +0300

    update UserRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserRepository.java
@@ -17,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByPhone(String phone);
+
+    boolean existsByPhone(String phone);
 }
