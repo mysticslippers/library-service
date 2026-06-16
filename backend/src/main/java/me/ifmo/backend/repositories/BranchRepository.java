10ca28e35e541f07aad58d0cff73e55b9d9d92ec
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 16:45:56 +0300

    add BranchRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/BranchRepository.java
@@ -0,0 +1,15 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Branch;
+import me.ifmo.backend.entities.enums.BranchStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+    List<Branch> findByLibrary_Id(Long libraryId);
+}
