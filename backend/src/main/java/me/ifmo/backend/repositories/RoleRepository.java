20860d57e143fe9f4db7f7f04ac7fd63b6b9d593
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:04:09 +0300

    add RoleRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Role;
+import me.ifmo.backend.entities.enums.RoleCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByCode(RoleCode code);
+}
