c417244041b579bc3ce2e1f78e7c31d726ee094b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:03:37 +0300

    update MaterialRepository

--- backend/src/main/java/me/ifmo/backend/repositories/MaterialRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialRepository.java
@@ -26,4 +26,21 @@ public interface MaterialRepository extends JpaRepository<Material, Long> {
     Page<Material> findByPublicationYear(Integer publicationYear, Pageable pageable);
 
     Page<Material> findByPublisherContainingIgnoreCase(String publisher, Pageable pageable);
+
+    @Query("""
+           SELECT material FROM Material material
+               WHERE (:query IS NULL OR :query = ''
+                   OR material.isbn LIKE concat('%', :query, '%')
+                   OR lower(material.title) LIKE lower(concat('%', :query, '%'))
+                   OR lower(coalesce(material.description, '')) LIKE lower(concat('%', :query, '%'))
+                   OR lower(coalesce(material.publisher, '')) LIKE lower(concat('%', :query, '%')))
+                   AND (:type IS NULL OR material.materialType = :type)
+                   AND (:status IS NULL OR material.status = :status)
+                   AND (:publicationYear IS NULL OR material.publicationYear = :publicationYear)
+    """)
+    Page<Material> search(@Param("query") String query,
+                          @Param("type") MaterialType type,
+                          @Param("status") MaterialStatus status,
+                          @Param("publicationYear") Integer publicationYear,
+                          Pageable pageable);
 }
