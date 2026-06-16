36190ab3fe892802854e2cbe2bf4ab9bf414d122
misticslippers <eternal.martyr13579@gmail.com>
2026-06-16 17:01:00 +0300

    add MaterialRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialRepository.java
@@ -0,0 +1,17 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.entities.enums.MaterialStatus;
+import me.ifmo.backend.entities.enums.MaterialType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+
+    Optional<Material> findByIsbn(String isbn);
+}
