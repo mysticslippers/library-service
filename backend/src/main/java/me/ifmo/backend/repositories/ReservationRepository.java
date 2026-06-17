1f3ac34098c4b530c586fd6f3f9f72b194395217
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:22:18 +0300

    update ReservationRepository

--- backend/src/main/java/me/ifmo/backend/repositories/ReservationRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/ReservationRepository.java
@@ -17,4 +17,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     Page<Reservation> findByUser_IdAndStatusIn(Long userId, Collection<ReservationStatus> statuses, Pageable pageable);
 
     Page<Reservation> findByMaterial_IdAndStatus(Long materialId, ReservationStatus status, Pageable pageable);
+
+    Page<Reservation> findByBranch_IdAndStatus(Long branchId, ReservationStatus status, Pageable pageable);
 }
