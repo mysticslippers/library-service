bb823167f2d5957647bfa884c67e0a6719345353
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:44:04 +0300

    update FineRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface FineRepository extends JpaRepository<Fine, Long> {
 
     Page<Fine> findByUser_Id(Long userId, Pageable pageable);
 
     Page<Fine> findByUser_IdAndStatus(Long userId, FineStatus status, Pageable pageable);
+
+    Page<Fine> findByUser_IdAndStatusIn(Long userId, Collection<FineStatus> statuses, Pageable pageable);
 }
