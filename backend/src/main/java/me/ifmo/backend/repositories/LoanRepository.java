c8350f71b2d9c451022be0db9f3b0201477f6363
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:29:18 +0300

    update LoanRepository

--- backend/src/main/java/me/ifmo/backend/repositories/LoanRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/LoanRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     Page<Loan> findByUser_Id(Long userId, Pageable pageable);
 
     Page<Loan> findByUser_IdAndStatus(Long userId, LoanStatus status, Pageable pageable);
+
+    Page<Loan> findByUser_IdAndStatusIn(Long userId, Collection<LoanStatus> statuses, Pageable pageable);
 }
