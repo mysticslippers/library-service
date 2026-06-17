cb4279af528b0acaf7d854501b42dde651a9f769
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:59:58 +0300

    update AuditLogRepository

--- backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
 
     Page<AuditLog> findByUser_Id(Long userId, Pageable pageable);
@@ -18,4 +20,6 @@ public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
     Page<AuditLog> findByAction(AuditAction action, Pageable pageable);
 
     Page<AuditLog> findByTypeAndAction(AuditEntityType type, AuditAction action, Pageable pageable);
+
+    Page<AuditLog> findByCreatedAtBetween(LocalDateTime from, LocalDateTime to, Pageable pageable);
 }
