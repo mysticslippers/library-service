681cc8b52a3e52617289fc6fed2a4de13dfed0d1
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:19:50 +0300

    update ReservationRepository

--- backend/src/main/java/me/ifmo/backend/repositories/ReservationRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/ReservationRepository.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Reservation;
+import me.ifmo.backend.entities.enums.ReservationStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     Page<Reservation> findByUser_Id(Long userId, Pageable pageable);
+
+    Page<Reservation> findByUser_IdAndStatus(Long userId, ReservationStatus status, Pageable pageable);
 }
