680f4e592473920f9f5c5eeb12c866fe149b1f41
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:47:20 +0300

    add PaymentTransactionRepository

---
+++ backend/src/main/java/me/ifmo/backend/repositories/PaymentTransactionRepository.java
@@ -0,0 +1,11 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.PaymentTransaction;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface PaymentTransactionRepository extends CrudRepository<PaymentTransaction, Long> {
+
+    Optional<PaymentTransaction> findByExternalPayment(String externalPayment);
+}
