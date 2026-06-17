15417f54261637f5ac0eaedfad6d374eacf19279
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:58:57 +0300

    update AuditLogRepository

--- backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.AuditLog;
+import me.ifmo.backend.entities.enums.AuditAction;
 import me.ifmo.backend.entities.enums.AuditEntityType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,4 +14,6 @@ public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
     Page<AuditLog> findByType(AuditEntityType type, Pageable pageable);
 
     Page<AuditLog> findByTypeAndEntityId(AuditEntityType type, Long entityId, Pageable pageable);
+
+    Page<AuditLog> findByAction(AuditAction action, Pageable pageable);
 }
