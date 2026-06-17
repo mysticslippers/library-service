b379aabc6bf29f80424b26095c0978225d568cff
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:48:06 +0300

    update PaymentTransactionRepository

--- backend/src/main/java/me/ifmo/backend/repositories/PaymentTransactionRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/PaymentTransactionRepository.java
@@ -1,6 +1,8 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.PaymentTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -10,4 +12,6 @@ public interface PaymentTransactionRepository extends CrudRepository<PaymentTran
     Optional<PaymentTransaction> findByExternalPayment(String externalPayment);
 
     boolean existsByExternalPayment(String externalPayment);
+
+    Page<PaymentTransaction> findByFine_Id(Long fineId, Pageable pageable);
 }
