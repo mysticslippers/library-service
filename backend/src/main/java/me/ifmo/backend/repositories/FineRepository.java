149451329bf3fe652ba6cc38cc2d2512887173f3
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:45:10 +0300

    update FineRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
@@ -2,12 +2,14 @@ package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Fine;
 import me.ifmo.backend.entities.enums.FineStatus;
+import me.ifmo.backend.entities.enums.ViolationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface FineRepository extends JpaRepository<Fine, Long> {
 
@@ -18,4 +20,6 @@ public interface FineRepository extends JpaRepository<Fine, Long> {
     Page<Fine> findByUser_IdAndStatusIn(Long userId, Collection<FineStatus> statuses, Pageable pageable);
 
     List<Fine> findByLoan_Id(Long loanId);
+
+    Optional<Fine> findByLoan_IdAndReasonAndStatus(Long loanId, ViolationType reason, FineStatus status);
 }
