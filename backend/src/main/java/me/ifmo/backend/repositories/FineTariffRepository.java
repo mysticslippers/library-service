ac23b1a09bd2cf2c61c64fa670f909b73dc17731
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:37:09 +0300

    update FineTariffRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineTariffRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineTariffRepository.java
@@ -3,6 +3,8 @@ package me.ifmo.backend.repositories;
 import me.ifmo.backend.entities.FineTariff;
 import me.ifmo.backend.entities.enums.FineTariffStatus;
 import me.ifmo.backend.entities.enums.ViolationType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface FineTariffRepository extends JpaRepository<FineTariff, Long> {
 
     Optional<FineTariff> findByViolationTypeAndStatus(ViolationType violationType, FineTariffStatus status);
+
+    Page<FineTariff> findByViolationType(ViolationType violationType, Pageable pageable);
 }
