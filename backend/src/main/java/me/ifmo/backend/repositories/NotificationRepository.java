314406dc2a2b23a1844fced257f6df94abe34e08
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:52:05 +0300

    update NotificationRepository

--- backend/src/main/java/me/ifmo/backend/repositories/NotificationRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/NotificationRepository.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Notification;
+import me.ifmo.backend.entities.enums.NotificationStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     Page<Notification> findByUser_Id(Long userId, Pageable pageable);
+
+    Page<Notification> findByUser_IdAndStatus(Long userId, NotificationStatus status, Pageable pageable);
 }
