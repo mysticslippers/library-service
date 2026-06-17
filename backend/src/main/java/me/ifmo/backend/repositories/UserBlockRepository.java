1a5d222cba64d76adbb7deec3056eafb78547a05
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 15:02:24 +0300

    update UserBlockRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
@@ -1,11 +1,16 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.UserBlock;
+import me.ifmo.backend.entities.enums.UserBlockStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserBlockRepository extends JpaRepository<UserBlock, Long> {
 
     Page<UserBlock> findByUser_Id(Long userId, Pageable pageable);
+
+    Optional<UserBlock> findByUser_IdAndStatus(Long userId, UserBlockStatus status);
 }
