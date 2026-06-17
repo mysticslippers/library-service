5323c63c28af3bf18a466b9cb81431986b1a7d2b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-17 14:41:31 +0300

    update FineTariffRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineTariffRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineTariffRepository.java
@@ -6,14 +6,29 @@ import me.ifmo.backend.entities.enums.ViolationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface FineTariffRepository extends JpaRepository<FineTariff, Long> {
 
     Page<FineTariff> findByStatus(FineTariffStatus status, Pageable pageable);
 
+    Page<FineTariff> findByViolationType(ViolationType violationType, Pageable pageable);
+
     Optional<FineTariff> findByViolationTypeAndStatus(ViolationType violationType, FineTariffStatus status);
 
-    Page<FineTariff> findByViolationType(ViolationType violationType, Pageable pageable);
+    @Query("""
+            SELECT fineTariff FROM FineTariff fineTariff
+                WHERE fineTariff.violationType = :violationType
+                    AND fineTariff.status = :status
+                    AND fineTariff.validFrom <= :dateTime
+                    AND (fineTariff.validTo IS NULL OR fineTariff.validTo > :dateTime)
+                ORDER BY fineTariff.validFrom DESC
+    """)
+    Optional<FineTariff> findActualByViolationTypeAndStatus(@Param("violationType") ViolationType violationType,
+                                                            @Param("status") FineTariffStatus status,
+                                                            @Param("dateTime") LocalDateTime dateTime);
 }
