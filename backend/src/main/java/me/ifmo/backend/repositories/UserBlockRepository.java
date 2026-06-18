c6e8959819904ac632679b6329927d53180aafac
misticslippers <eternal.martyr13579@gmail.com>
2026-06-18 12:10:51 +0300

    update UserBlockRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserBlockRepository extends JpaRepository<UserBlock, Long> {
@@ -19,4 +20,6 @@ public interface UserBlockRepository extends JpaRepository<UserBlock, Long> {
     Page<UserBlock> findByStatus(UserBlockStatus status, Pageable pageable);
 
     Page<UserBlock> findByCreatedByUser_Id(Long createdByUserId, Pageable pageable);
+
+    Page<UserBlock> findByBlockedAtBetween(LocalDateTime from, LocalDateTime to, Pageable pageable);
 }
