e046979fbad46be26cecf09c175ae3ea94cd053e
misticslippers <eternal.martyr13579@gmail.com>
2026-06-18 19:35:56 +0300

    add CreateUserWarningRequest

---
+++ backend/src/main/java/me/ifmo/backend/DTO/user/request/CreateUserWarningRequest.java
@@ -0,0 +1,22 @@
+package me.ifmo.backend.DTO.user.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record CreateUserWarningRequest(
+        @NotNull
+        Long userId,
+
+        @NotBlank
+        @Size(max = 2000)
+        String reason,
+
+        @Size(max = 2000)
+        String comment,
+
+        LocalDateTime expiresAt
+) {
+}
