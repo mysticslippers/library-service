6b0fb7d6d0feb9aed8e4d17f306890c1a365c34d
misticslippers <eternal.martyr13579@gmail.com>
2026-06-18 19:35:40 +0300

    add CreateUserRequest

---
+++ backend/src/main/java/me/ifmo/backend/DTO/user/request/CreateUserRequest.java
@@ -0,0 +1,39 @@
+package me.ifmo.backend.DTO.user.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import me.ifmo.backend.entities.enums.RoleCode;
+
+import java.util.Set;
+
+public record CreateUserRequest(
+        @Email
+        @NotBlank
+        @Size(max = 255)
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "^(\\+7|8)[0-9]{10}$")
+        String phone,
+
+        @NotBlank
+        @Size(min = 8, max = 255)
+        String password,
+
+        @NotBlank
+        @Size(max = 100)
+        String firstName,
+
+        @NotBlank
+        @Size(max = 100)
+        String lastName,
+
+        @Size(max = 100)
+        String middleName,
+
+        Long homeBranchId,
+        Set<RoleCode> roles
+) {
+}
