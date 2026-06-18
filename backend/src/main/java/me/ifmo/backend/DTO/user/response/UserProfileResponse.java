474a0d2adc14c4344ef8a765d82ea250a1c3d3ae
misticslippers <eternal.martyr13579@gmail.com>
2026-06-18 19:37:11 +0300

    add UserProfileResponse

---
+++ backend/src/main/java/me/ifmo/backend/DTO/user/response/UserProfileResponse.java
@@ -0,0 +1,16 @@
+package me.ifmo.backend.DTO.user.response;
+
+import me.ifmo.backend.entities.enums.UserStatus;
+
+public record UserProfileResponse(
+        Long id,
+        String email,
+        String phone,
+        String firstName,
+        String lastName,
+        String middleName,
+        UserStatus status,
+        Long homeBranchId,
+        String homeBranchName
+) {
+}
