166a7a9181177e0c1c007f98a6e989fd624fd404
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:26:39 +0300

    add UpdateMaterialRequest

---
+++ backend/src/main/java/me/ifmo/backend/dto/catalog/request/UpdateMaterialRequest.java
@@ -0,0 +1,42 @@
+package me.ifmo.backend.dto.catalog.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import me.ifmo.backend.entities.enums.MaterialStatus;
+import me.ifmo.backend.entities.enums.MaterialType;
+
+import java.util.List;
+import java.util.Set;
+
+public record UpdateMaterialRequest(
+        @Pattern(regexp = "^[0-9]{10}([0-9]{3})?$")
+        String isbn,
+
+        @Size(max = 255)
+        String title,
+
+        String description,
+
+        @Size(max = 255)
+        String publisher,
+
+        @Min(1000)
+        @Max(3000)
+        Integer publicationYear,
+
+        MaterialType materialType,
+
+        @Size(max = 50)
+        String language,
+
+        MaterialStatus status,
+
+        @Valid
+        List<MaterialAuthorRequest> authors,
+
+        Set<Long> genreIds
+) {
+}
