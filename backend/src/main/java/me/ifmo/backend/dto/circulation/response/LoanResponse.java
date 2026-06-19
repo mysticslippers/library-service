3939382878267924a5a383a374699d9875511e02
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:22:06 +0300

    add LoanResponse

---
+++ backend/src/main/java/me/ifmo/backend/dto/circulation/response/LoanResponse.java
@@ -0,0 +1,22 @@
+package me.ifmo.backend.dto.circulation.response;
+
+import me.ifmo.backend.dto.catalog.response.MaterialCopyResponse;
+import me.ifmo.backend.dto.library.response.BranchShortResponse;
+import me.ifmo.backend.dto.user.response.UserShortResponse;
+import me.ifmo.backend.entities.enums.LoanStatus;
+
+import java.time.LocalDateTime;
+
+public record LoanResponse(
+        Long id,
+        UserShortResponse user,
+        MaterialCopyResponse copy,
+        Long reservationId,
+        BranchShortResponse branch,
+        UserShortResponse issuedByUser,
+        LocalDateTime loanedAt,
+        LocalDateTime dueAt,
+        LocalDateTime returnedAt,
+        LoanStatus status
+) {
+}
