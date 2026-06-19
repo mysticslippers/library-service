42ebdb72e6836f2ee7975f863216b23ef111a6c4
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:18:34 +0300

    add CreateAuthorRequest

---
+++ backend/src/main/java/me/ifmo/backend/dto/catalog/request/CreateAuthorRequest.java
@@ -0,0 +1,18 @@
+package me.ifmo.backend.dto.catalog.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateAuthorRequest(
+        @NotBlank
+        @Size(max = 100)
+        String firstName,
+
+        @NotBlank
+        @Size(max = 100)
+        String lastName,
+
+        @Size(max = 100)
+        String middleName
+) {
+}
