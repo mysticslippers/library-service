e35f34fdee2c23b1dce13140794f22af55520859
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:19:12 +0300

    add CreateLibraryRequest

---
+++ backend/src/main/java/me/ifmo/backend/dto/library/request/CreateLibraryRequest.java
@@ -0,0 +1,15 @@
+package me.ifmo.backend.dto.library.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateLibraryRequest(
+        @NotBlank
+        @Size(max = 50)
+        String code,
+
+        @NotBlank
+        @Size(max = 255)
+        String name
+) {
+}
