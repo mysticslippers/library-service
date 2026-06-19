77cc307226b182a657aa9e5c9bd515e35aa8171f
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:35:06 +0300

    add CreateFineTariffRequest

---
+++ backend/src/main/java/me/ifmo/backend/dto/fine/request/CreateFineTariffRequest.java
@@ -0,0 +1,26 @@
+package me.ifmo.backend.dto.fine.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import me.ifmo.backend.entities.enums.ViolationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record CreateFineTariffRequest(
+        @NotNull
+        ViolationType violationType,
+
+        @DecimalMin("0.00")
+        BigDecimal amountPerDay,
+
+        @DecimalMin("0.00")
+        BigDecimal fixedAmount,
+
+        @DecimalMin("0.00")
+        BigDecimal maxAmount,
+
+        LocalDateTime validFrom,
+        LocalDateTime validTo
+) {
+}
