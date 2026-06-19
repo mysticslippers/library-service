868d0eaf1e3cd3de9caab24f4f64e7e42ed1aa4a
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:23:17 +0300

    add MaterialResponse

---
+++ backend/src/main/java/me/ifmo/backend/dto/catalog/response/MaterialResponse.java
@@ -0,0 +1,21 @@
+package me.ifmo.backend.dto.catalog.response;
+
+import me.ifmo.backend.entities.enums.MaterialStatus;
+import me.ifmo.backend.entities.enums.MaterialType;
+
+import java.util.List;
+
+public record MaterialResponse(
+        Long id,
+        String isbn,
+        String title,
+        String description,
+        String publisher,
+        Integer publicationYear,
+        MaterialType materialType,
+        String language,
+        MaterialStatus status,
+        List<MaterialAuthorResponse> authors,
+        List<GenreResponse> genres
+) {
+}
