ddb3e7b689d029ea614fbe590a53513c8194ef14
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:21:37 +0300

    add LibraryRuleResponse

---
+++ backend/src/main/java/me/ifmo/backend/dto/library/response/LibraryRuleResponse.java
@@ -0,0 +1,22 @@
+package me.ifmo.backend.dto.library.response;
+
+import me.ifmo.backend.entities.enums.LibraryRuleStatus;
+
+import java.time.LocalDateTime;
+
+public record LibraryRuleResponse(
+        Long id,
+        BranchShortResponse branch,
+        Integer maxActiveReservations,
+        Integer maxActiveLoans,
+        Integer reservationTtlDays,
+        Integer defaultLoanDays,
+        Boolean renewalAllowed,
+        Integer maxRenewalCount,
+        Integer renewalPeriodDays,
+        Boolean reservationAllowed,
+        LibraryRuleStatus status,
+        LocalDateTime validFrom,
+        LocalDateTime validTo
+) {
+}
