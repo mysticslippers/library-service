67861fc4c6ca77b05c1f1135a32ee02bc3043319
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:24:46 +0300

    add ReturnLoanRequest

---
+++ backend/src/main/java/me/ifmo/backend/dto/circulation/request/ReturnLoanRequest.java
@@ -0,0 +1,12 @@
+package me.ifmo.backend.dto.circulation.request;
+
+import jakarta.validation.constraints.Size;
+import me.ifmo.backend.entities.enums.CopyStatus;
+
+public record ReturnLoanRequest(
+        CopyStatus resultingCopyStatus,
+
+        @Size(max = 1000)
+        String comment
+) {
+}
