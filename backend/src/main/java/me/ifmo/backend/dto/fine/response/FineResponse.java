681993a861e564d394e0f5ecac4600f53effae1b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-19 16:35:38 +0300

    add FineResponse

---
+++ backend/src/main/java/me/ifmo/backend/dto/fine/response/FineResponse.java
@@ -0,0 +1,24 @@
+package me.ifmo.backend.dto.fine.response;
+
+import me.ifmo.backend.dto.catalog.response.MaterialCopyResponse;
+import me.ifmo.backend.dto.user.response.UserShortResponse;
+import me.ifmo.backend.entities.enums.FineStatus;
+import me.ifmo.backend.entities.enums.ViolationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record FineResponse(
+        Long id,
+        UserShortResponse user,
+        Long loanId,
+        MaterialCopyResponse copy,
+        Long tariffId,
+        ViolationType reason,
+        BigDecimal amount,
+        FineStatus status,
+        LocalDateTime createdAt,
+        LocalDateTime paidAt,
+        LocalDateTime cancelledAt
+) {
+}
