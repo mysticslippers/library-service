3c102a5fc0f6a94ade41a9834bde51feca020b99
misticslippers <eternal.martyr13579@gmail.com>
2026-06-20 11:00:35 +0300

    add AuditLogResponse

---
+++ backend/src/main/java/me/ifmo/backend/dto/audit/response/AuditLogResponse.java
@@ -0,0 +1,19 @@
+package me.ifmo.backend.dto.audit.response;
+
+import me.ifmo.backend.dto.user.response.UserShortResponse;
+import me.ifmo.backend.entities.enums.AuditAction;
+import me.ifmo.backend.entities.enums.AuditEntityType;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public record AuditLogResponse(
+        Long id,
+        UserShortResponse actor,
+        AuditEntityType type,
+        Long entityId,
+        AuditAction action,
+        Map<String, Object> details,
+        LocalDateTime createdAt
+) {
+}
