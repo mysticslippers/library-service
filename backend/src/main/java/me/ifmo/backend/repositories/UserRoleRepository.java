4bb0421612635b63064ae30f6f01dc167941ef89
misticslippers <eternal.martyr13579@gmail.com>
2026-06-20 11:40:38 +0300

    update UserRoleRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserRoleRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserRoleRepository.java
@@ -2,6 +2,7 @@ package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.UserRole;
 import me.ifmo.backend.entities.enums.RoleCode;
+import me.ifmo.backend.entities.id.UserRoleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
 
     List<UserRole> findByUser_Id(Long userId);
 
