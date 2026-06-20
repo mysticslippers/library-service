f2c80f148db6e97b12572e06226bf54dd4c06018
misticslippers <eternal.martyr13579@gmail.com>
2026-06-20 12:23:33 +0300

    update LibraryMapper

--- backend/src/main/java/me/ifmo/backend/mappers/LibraryMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/LibraryMapper.java
@@ -1,11 +1,9 @@
 package me.ifmo.backend.mappers;
 
 import me.ifmo.backend.dto.library.request.CreateLibraryRequest;
+import me.ifmo.backend.dto.library.request.UpdateLibraryRequest;
 import me.ifmo.backend.entities.Library;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface LibraryMapper {
@@ -14,4 +12,10 @@ public interface LibraryMapper {
     @Mapping(target = "code", source = "code")
     @Mapping(target = "name", source = "name")
     Library toEntity(CreateLibraryRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    void updateEntity(UpdateLibraryRequest request, @MappingTarget Library library);
 }
