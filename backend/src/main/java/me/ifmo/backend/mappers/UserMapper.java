8ff6fdcdd4ce5f3e528c971fa5b488d6c8169532
misticslippers <eternal.martyr13579@gmail.com>
2026-06-20 11:52:42 +0300

    update UserMapper

--- backend/src/main/java/me/ifmo/backend/mappers/UserMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.dto.auth.request.RegisterRequest;
 import me.ifmo.backend.dto.user.request.CreateUserRequest;
 import me.ifmo.backend.entities.User;
 import org.mapstruct.BeanMapping;
@@ -18,4 +19,12 @@ public interface UserMapper {
         @Mapping(target = "lastName", source = "lastName")
         @Mapping(target = "middleName", source = "middleName")
         User toEntity(CreateUserRequest request);
+
+        @BeanMapping(ignoreByDefault = true)
+        @Mapping(target = "email", source = "email")
+        @Mapping(target = "phone", source = "phone")
+        @Mapping(target = "firstName", source = "firstName")
+        @Mapping(target = "lastName", source = "lastName")
+        @Mapping(target = "middleName", source = "middleName")
+        User toEntity(RegisterRequest request);
 }
