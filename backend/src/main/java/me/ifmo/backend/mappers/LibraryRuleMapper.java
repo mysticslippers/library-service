e03887d2a2bda847fe1706ec5b4aa07a2b370c3f
misticslippers <eternal.martyr13579@gmail.com>
2026-06-21 16:37:33 +0300

    update LibraryRuleMapper

--- backend/src/main/java/me/ifmo/backend/mappers/LibraryRuleMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/LibraryRuleMapper.java
@@ -1,9 +1,20 @@
 package me.ifmo.backend.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueMappingStrategy;
+import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
+import me.ifmo.backend.entities.Branch;
+import me.ifmo.backend.entities.LibraryRule;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
 
 @Mapper(uses = BranchMapper.class,
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface LibraryRuleMapper {
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "branch", source = "branch")
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "validFrom", source = "validFrom")
+    LibraryRule toEntity(CreateLibraryRuleRequest request, Branch branch, LocalDateTime validFrom);
 }
