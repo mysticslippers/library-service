6f036ae050b529526a255132beb8dcb59fa5bbd1
misticslippers <eternal.martyr13579@gmail.com>
2026-06-21 16:36:50 +0300

    add LibraryRuleMapper

---
+++ backend/src/main/java/me/ifmo/backend/mappers/LibraryRuleMapper.java
@@ -0,0 +1,9 @@
+package me.ifmo.backend.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
+
+@Mapper(uses = BranchMapper.class,
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface LibraryRuleMapper {
+}
