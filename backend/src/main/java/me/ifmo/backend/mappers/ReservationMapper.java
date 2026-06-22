72aa8a287e3679b59235c5f3cf9f49460e536f72
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:46:37 +0300

    update ReservationMapper

--- backend/src/main/java/me/ifmo/backend/mappers/ReservationMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/ReservationMapper.java
@@ -1,5 +1,7 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.dto.catalog.response.MaterialShortResponse;
+import me.ifmo.backend.dto.circulation.response.ReservationResponse;
 import me.ifmo.backend.entities.*;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
@@ -17,4 +19,9 @@ public interface ReservationMapper {
     @Mapping(target = "branch", source = "branch")
     @Mapping(target = "expiresAt", source = "expiresAt")
     Reservation toEntity(User user, Material material, MaterialCopy copy, Branch branch, LocalDateTime expiresAt);
+
+    @Mapping(target = "id", source = "reservation.id")
+    @Mapping(target = "material", source = "materialResponse")
+    @Mapping(target = "status", source = "reservation.status")
+    ReservationResponse toResponse(Reservation reservation, MaterialShortResponse materialResponse);
 }
