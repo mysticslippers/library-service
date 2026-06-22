b36a7af03521f438ef957dd2b34a563396d27e22
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:49:37 +0300

    update LoanMapper

--- backend/src/main/java/me/ifmo/backend/mappers/LoanMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/LoanMapper.java
@@ -1,4 +1,23 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.entities.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
+
+import java.time.LocalDateTime;
+
+@Mapper(uses = {UserMapper.class, MaterialCopyMapper.class, BranchMapper.class},
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface LoanMapper {
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "copy", source = "copy")
+    @Mapping(target = "reservation", source = "reservation")
+    @Mapping(target = "branch", source = "branch")
+    @Mapping(target = "issuedByUser", source = "issuedByUser")
+    @Mapping(target = "dueAt", source = "dueAt")
+    Loan toEntity(User user, MaterialCopy copy, Reservation reservation, Branch branch, User issuedByUser, LocalDateTime dueAt);
 }
