3651cf4d81c6c7eb15f44eb75bc792660728ff58
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 17:01:28 +0300

    update FineMapper

--- backend/src/main/java/me/ifmo/backend/mappers/FineMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/FineMapper.java
@@ -1,10 +1,24 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.entities.*;
+import me.ifmo.backend.entities.enums.ViolationType;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValueMappingStrategy;
+
+import java.math.BigDecimal;
 
 @Mapper(uses = {UserMapper.class, MaterialCopyMapper.class},
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface FineMapper {
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "loan", source = "loan")
+    @Mapping(target = "copy", source = "copy")
+    @Mapping(target = "tariff", source = "tariff")
+    @Mapping(target = "reason", source = "reason")
+    @Mapping(target = "amount", source = "amount")
+    Fine toEntity(User user, Loan loan, MaterialCopy copy, FineTariff tariff, ViolationType reason, BigDecimal amount);
 }
