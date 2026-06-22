5594d6a59a128c4df808821c6280b2cbc11581ac
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 15:57:32 +0300

    update MaterialCopyMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialCopyMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialCopyMapper.java
@@ -1,4 +1,17 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.dto.catalog.request.CreateMaterialCopyRequest;
+import me.ifmo.backend.entities.MaterialCopy;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
+
+@Mapper(nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface MaterialCopyMapper {
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "inventoryNumber", source = "inventoryNumber")
+    @Mapping(target = "shelfLocation", source = "shelfLocation")
+    MaterialCopy toEntity(CreateMaterialCopyRequest request);
 }
