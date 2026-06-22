96866ef5732eb6cb2add6211f9c47a5d3a2fef7f
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:42:05 +0300

    update MaterialMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialMapper.java
@@ -2,8 +2,13 @@ package me.ifmo.backend.mappers;
 
 import me.ifmo.backend.dto.catalog.request.CreateMaterialRequest;
 import me.ifmo.backend.dto.catalog.request.UpdateMaterialRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialResponse;
 import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.entities.MaterialAuthor;
+import me.ifmo.backend.entities.MaterialGenre;
 import org.mapstruct.*;
+
+import java.util.Collection;
 
 @Mapper(uses = {MaterialAuthorMapper.class, MaterialGenreMapper.class})
 public interface MaterialMapper {
@@ -20,4 +25,9 @@ public interface MaterialMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     void updateEntity(UpdateMaterialRequest request, @MappingTarget Material material);
+
+    @Mapping(target = "authors", source = "materialAuthors")
+    @Mapping(target = "genres", source = "materialGenres")
+    MaterialResponse toResponse(Material material, Collection<MaterialAuthor> materialAuthors,
+                                Collection<MaterialGenre> materialGenres);
 }
