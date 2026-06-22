f0c63ffac7a86c17406354c8674332b5ab27e4b3
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:38:20 +0300

    update MaterialAuthorMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialAuthorMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialAuthorMapper.java
@@ -1,9 +1,11 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.dto.catalog.response.AuthorResponse;
 import me.ifmo.backend.dto.catalog.response.MaterialAuthorResponse;
 import me.ifmo.backend.entities.Author;
 import me.ifmo.backend.entities.Material;
 import me.ifmo.backend.entities.MaterialAuthor;
+import me.ifmo.backend.entities.id.MaterialAuthorId;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,6 +15,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Mapper(uses = AuthorMapper.class,
+        imports = MaterialAuthorId.class,
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface MaterialAuthorMapper {
 
@@ -26,4 +29,9 @@ public interface MaterialAuthorMapper {
     MaterialAuthorResponse toResponse(MaterialAuthor materialAuthor);
 
     List<MaterialAuthorResponse> toResponseList(Collection<MaterialAuthor> materialAuthors);
+
+    @Mapping(target = ".", source = "author")
+    AuthorResponse toAuthorResponse(MaterialAuthor materialAuthor);
+
+    List<AuthorResponse> toAuthorResponseList(Collection<MaterialAuthor> materialAuthors);
 }
