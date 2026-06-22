f5a232c8627250eee8ecdcc70ab10ba9c4fc7cd4
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:41:25 +0300

    update MaterialMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialMapper.java
@@ -1,11 +1,9 @@
 package me.ifmo.backend.mappers;
 
 import me.ifmo.backend.dto.catalog.request.CreateMaterialRequest;
+import me.ifmo.backend.dto.catalog.request.UpdateMaterialRequest;
 import me.ifmo.backend.entities.Material;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.*;
 
 @Mapper(uses = {MaterialAuthorMapper.class, MaterialGenreMapper.class})
 public interface MaterialMapper {
@@ -16,4 +14,10 @@ public interface MaterialMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Material toEntity(CreateMaterialRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntity(UpdateMaterialRequest request, @MappingTarget Material material);
 }
