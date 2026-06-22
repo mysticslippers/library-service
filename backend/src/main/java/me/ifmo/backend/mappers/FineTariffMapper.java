85b0cc89ab3901f65b4a4d87d9fe18e8625eb534
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:53:14 +0300

    update FineTariffMapper

--- backend/src/main/java/me/ifmo/backend/mappers/FineTariffMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/FineTariffMapper.java
@@ -1,4 +1,17 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
+import me.ifmo.backend.entities.FineTariff;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+
+@Mapper(nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface FineTariffMapper {
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "validFrom", source = "validFrom")
+    FineTariff toEntity(CreateFineTariffRequest request, LocalDateTime validFrom);
 }
