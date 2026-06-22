11a8dc39c5f6709c26b2f6828f030fa09ad580d6
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:09:05 +0300

    update MaterialAuthorMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialAuthorMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialAuthorMapper.java
@@ -1,10 +1,21 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.entities.Author;
+import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.entities.MaterialAuthor;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValueMappingStrategy;
 
 @Mapper(uses = AuthorMapper.class,
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface MaterialAuthorMapper {
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", expression = "java(new MaterialAuthorId(material.getId(), author.getId()))")
+    @Mapping(target = "material", source = "material")
+    @Mapping(target = "author", source = "author")
+    @Mapping(target = "authorOrder", source = "authorOrder")
+    MaterialAuthor toEntity(Material material, Author author, Integer authorOrder);
 }
