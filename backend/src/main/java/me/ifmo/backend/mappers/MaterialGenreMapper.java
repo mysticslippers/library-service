25f70e3f16f7e5002229382fafcd19a6db621187
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:11:41 +0300

    add MaterialGenreMapper

---
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialGenreMapper.java
@@ -0,0 +1,10 @@
+package me.ifmo.backend.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
+
+@Mapper(uses = GenreMapper.class,
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface MaterialGenreMapper {
+
+}
