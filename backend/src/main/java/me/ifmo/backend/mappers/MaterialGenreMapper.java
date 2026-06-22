1c045738adb1780fb0f5fdd1ce69570a90542253
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 16:14:43 +0300

    update MaterialGenreMapper

--- backend/src/main/java/me/ifmo/backend/mappers/MaterialGenreMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/MaterialGenreMapper.java
@@ -1,10 +1,20 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.entities.Genre;
+import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.entities.MaterialGenre;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValueMappingStrategy;
 
 @Mapper(uses = GenreMapper.class,
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface MaterialGenreMapper {
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", expression = "java(new MaterialGenreId(material.getId(), genre.getId()))")
+    @Mapping(target = "material", source = "material")
+    @Mapping(target = "genre", source = "genre")
+    MaterialGenre toEntity(Material material, Genre genre);
 }
