b5a7f0876b9e6eb29c57556aacefcdcdd12bc2d8
misticslippers <eternal.martyr13579@gmail.com>
2026-06-22 17:05:48 +0300

    update PaymentTransactionMapper

--- backend/src/main/java/me/ifmo/backend/mappers/PaymentTransactionMapper.java
+++ backend/src/main/java/me/ifmo/backend/mappers/PaymentTransactionMapper.java
@@ -1,4 +1,20 @@
 package me.ifmo.backend.mappers;
 
+import me.ifmo.backend.entities.Fine;
+import me.ifmo.backend.entities.PaymentTransaction;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
+
+import java.math.BigDecimal;
+
+@Mapper(nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface PaymentTransactionMapper {
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "fine", source = "fine")
+    @Mapping(target = "externalPayment", source = "externalPayment")
+    @Mapping(target = "amount", source = "amount")
+    PaymentTransaction toEntity(Fine fine, String externalPayment, BigDecimal amount);
 }
