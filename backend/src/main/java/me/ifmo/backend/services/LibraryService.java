f807a813cce4c1d4dc9cd06bb85460fb2fc41132
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 19:30:31 +0300

    update LibraryService

--- backend/src/main/java/me/ifmo/backend/services/LibraryService.java
+++ backend/src/main/java/me/ifmo/backend/services/LibraryService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.library.request.CreateLibraryRequest;
+import me.ifmo.backend.dto.library.response.LibraryResponse;
+
 public interface LibraryService {
+
+    LibraryResponse create(CreateLibraryRequest request);
 }
