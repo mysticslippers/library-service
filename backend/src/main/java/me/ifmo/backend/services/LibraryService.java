625a4f8a7e0f5b29331ded910c73a12dfdd007b6
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 19:32:38 +0300

    update LibraryService

--- backend/src/main/java/me/ifmo/backend/services/LibraryService.java
+++ backend/src/main/java/me/ifmo/backend/services/LibraryService.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.library.request.ChangeLibraryStatusRequest;
 import me.ifmo.backend.dto.library.request.CreateLibraryRequest;
 import me.ifmo.backend.dto.library.request.UpdateLibraryRequest;
 import me.ifmo.backend.dto.library.response.LibraryResponse;
+import me.ifmo.backend.entities.enums.LibraryStatus;
+import org.springframework.data.domain.Pageable;
 
 public interface LibraryService {
 
@@ -16,4 +19,6 @@ public interface LibraryService {
     LibraryResponse update(Long id, UpdateLibraryRequest request);
 
     LibraryResponse changeStatus(Long id, ChangeLibraryStatusRequest request);
+
+    PageResponse<LibraryResponse> search(String query, LibraryStatus status, Pageable pageable);
 }
