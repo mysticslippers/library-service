9b02d2edd52fca4e9fd9dd8c2734cf133cd56c16
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 17:29:21 +0300

    add ResourceNotFoundException

---
+++ backend/src/main/java/me/ifmo/backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package me.ifmo.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends ApiException {
+    public ResourceNotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, "RESOURCE_NOT_FOUND", message);
+    }
+}
