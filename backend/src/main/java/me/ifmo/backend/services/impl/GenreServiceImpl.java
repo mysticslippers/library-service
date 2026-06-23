dc3c8c4e7bac814615e4bc3e569f52101d0c1889
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 18:25:00 +0300

    update GenreServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/GenreServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/GenreServiceImpl.java
@@ -5,6 +5,7 @@ import me.ifmo.backend.dto.catalog.request.CreateGenreRequest;
 import me.ifmo.backend.dto.catalog.request.UpdateGenreRequest;
 import me.ifmo.backend.dto.catalog.response.GenreResponse;
 import me.ifmo.backend.entities.Genre;
+import me.ifmo.backend.exceptions.domain.BusinessRuleException;
 import me.ifmo.backend.exceptions.domain.DuplicateResourceException;
 import me.ifmo.backend.exceptions.domain.ResourceNotFoundException;
 import me.ifmo.backend.mappers.GenreMapper;
@@ -62,7 +63,36 @@ public class GenreServiceImpl implements GenreService {
         return mapper.toResponse(genre);
     }
 
+    @Override
+    @Transactional
+    public GenreResponse update(Long id, UpdateGenreRequest request){
+        Genre genre = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No genre with id '%s' found".formatted(id)));
 
+        String normalizedName = (request.name() != null) ? request.name().strip() : null;
+        String normalizedCode = (request.code() != null) ? request.code().strip().toUpperCase(Locale.ROOT) : null;
+
+        if(normalizedName != null && normalizedName.isBlank())
+            throw new BusinessRuleException("Genre code must not be blank");
+
+        if(normalizedCode != null && normalizedCode.isBlank())
+            throw new BusinessRuleException("Genre code must not be blank");
+
+        if(normalizedName != null && normalizedName.equalsIgnoreCase(genre.getName())
+                && repository.existsByNameIgnoreCase(normalizedName))
+            throw new DuplicateResourceException("Genre with name '%s' already exists".formatted(normalizedName));
+
+        if(normalizedCode != null && normalizedCode.equalsIgnoreCase(genre.getCode())
+                && repository.existsByCode(normalizedCode))
+            throw new DuplicateResourceException("Genre with code '%s' already exists".formatted(normalizedCode));
+
+        UpdateGenreRequest normalizedRequest = new UpdateGenreRequest(normalizedCode, normalizedName);
+
+        mapper.updateEntity(normalizedRequest, genre);
+
+        Genre saved = repository.save(genre);
+        return mapper.toResponse(saved);
+    }
 
     @Override
     @Transactional
