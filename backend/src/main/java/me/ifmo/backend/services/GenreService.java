e7e5e59c9637e64564f640e98524c01944065028
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 16:58:49 +0300

    update GenreService

--- backend/src/main/java/me/ifmo/backend/services/GenreService.java
+++ backend/src/main/java/me/ifmo/backend/services/GenreService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.catalog.request.CreateGenreRequest;
+import me.ifmo.backend.dto.catalog.response.GenreResponse;
+
 public interface GenreService {
+
+    GenreResponse create(CreateGenreRequest request);
 }
