a35b14515ab8d3c70af8942539cc4345c2774c99
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 18:57:48 +0300

    update AuthorService

--- backend/src/main/java/me/ifmo/backend/services/AuthorService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuthorService.java
@@ -1,8 +1,11 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.catalog.request.AuthorSearchRequest;
 import me.ifmo.backend.dto.catalog.request.CreateAuthorRequest;
 import me.ifmo.backend.dto.catalog.request.UpdateAuthorRequest;
 import me.ifmo.backend.dto.catalog.response.AuthorResponse;
+import me.ifmo.backend.dto.common.response.PageResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface AuthorService {
 
@@ -11,4 +14,6 @@ public interface AuthorService {
     AuthorResponse getAuthorById(Long id);
 
     AuthorResponse update(Long id, UpdateAuthorRequest request);
+
+    PageResponse<AuthorResponse> search(AuthorSearchRequest request, Pageable pageable);
 }
