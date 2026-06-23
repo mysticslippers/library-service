056d69bd07b07a24ffabc844ce232532bb6e5b0b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-23 18:40:47 +0300

    update AuthorService

--- backend/src/main/java/me/ifmo/backend/services/AuthorService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuthorService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.catalog.request.CreateAuthorRequest;
+import me.ifmo.backend.dto.catalog.response.AuthorResponse;
+
 public interface AuthorService {
+
+    AuthorResponse create(CreateAuthorRequest request);
 }
