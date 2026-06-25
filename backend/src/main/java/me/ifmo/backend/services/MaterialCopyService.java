9850245af747fd722cc14e249d6b9f44878891ce
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 19:09:31 +0300

    update MaterialCopyService

--- backend/src/main/java/me/ifmo/backend/services/MaterialCopyService.java
+++ backend/src/main/java/me/ifmo/backend/services/MaterialCopyService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.catalog.request.CreateMaterialCopyRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialCopyResponse;
+
 public interface MaterialCopyService {
+
+    MaterialCopyResponse create(CreateMaterialCopyRequest request);
 }
