0c6906aa01312f2019d6db9a2883c98b1bc3019c
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 18:49:37 +0300

    update LibraryRuleService

--- backend/src/main/java/me/ifmo/backend/services/LibraryRuleService.java
+++ backend/src/main/java/me/ifmo/backend/services/LibraryRuleService.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
+import me.ifmo.backend.dto.library.request.UpdateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.response.LibraryRuleResponse;
 
 public interface LibraryRuleService {
@@ -10,4 +11,6 @@ public interface LibraryRuleService {
     LibraryRuleResponse getLibraryRuleById(Long id);
 
     LibraryRuleResponse getActualByBranchId(Long branchId);
+
+    LibraryRuleResponse update(Long id, UpdateLibraryRuleRequest request);
 }
