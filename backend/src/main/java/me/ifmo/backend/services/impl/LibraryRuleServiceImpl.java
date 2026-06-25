ab1c7a82587c5605a40886ecf53e5056f8c65451
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 18:57:10 +0300

    update LibraryRuleServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/LibraryRuleServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/LibraryRuleServiceImpl.java
@@ -2,6 +2,7 @@ package me.ifmo.backend.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
+import me.ifmo.backend.dto.library.request.UpdateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.response.LibraryRuleResponse;
 import me.ifmo.backend.entities.Branch;
 import me.ifmo.backend.entities.LibraryRule;
@@ -66,5 +67,23 @@ public class LibraryRuleServiceImpl implements LibraryRuleService {
                 new ResourceNotFoundException("Actual library rule for branch with id '%s' not found".formatted(branchId)));
 
         return mapper.toResponse(rule);
+    }
+
+    @Override
+    @Transactional
+    public LibraryRuleResponse update(Long id, UpdateLibraryRuleRequest request) {
+        LibraryRule rule = repository.findById(id).orElseThrow(() ->
+                        new ResourceNotFoundException("Library rule with id '%s' not found".formatted(id)));
+
+        if (rule.getStatus() == LibraryRuleStatus.ARCHIVED)
+            throw new BusinessRuleException("Archived library rule cannot be updated");
+
+        if (request.validTo() != null && !request.validTo().isAfter(rule.getValidFrom()))
+            throw new BusinessRuleException("Library rule validTo must be after validFrom");
+
+        mapper.updateEntity(request, rule);
+
+        LibraryRule saved = repository.save(rule);
+        return mapper.toResponse(saved);
     }
 }
