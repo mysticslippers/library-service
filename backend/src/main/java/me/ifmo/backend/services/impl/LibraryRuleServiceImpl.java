8e83f85de673da888350f7f75f9f6b6717615673
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 18:51:37 +0300

    update LibraryRuleServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/LibraryRuleServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/LibraryRuleServiceImpl.java
@@ -1,4 +1,17 @@
 package me.ifmo.backend.services.impl;
 
-public class LibraryRuleServiceImpl{
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.mappers.LibraryRuleMapper;
+import me.ifmo.backend.repositories.BranchRepository;
+import me.ifmo.backend.repositories.LibraryRuleRepository;
+import me.ifmo.backend.services.LibraryRuleService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LibraryRuleServiceImpl implements LibraryRuleService {
+
+    private final LibraryRuleRepository repository;
+    private final BranchRepository branchRepository;
+    private final LibraryRuleMapper mapper;
 }
