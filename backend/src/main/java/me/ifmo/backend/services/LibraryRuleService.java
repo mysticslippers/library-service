6fe0416983d5061b8cc0f50bc38a3f02392bfa2d
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 18:50:20 +0300

    update LibraryRuleService

--- backend/src/main/java/me/ifmo/backend/services/LibraryRuleService.java
+++ backend/src/main/java/me/ifmo/backend/services/LibraryRuleService.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.library.request.ChangeLibraryRuleStatusRequest;
 import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.request.UpdateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.response.LibraryRuleResponse;
+import me.ifmo.backend.entities.enums.LibraryRuleStatus;
+import org.springframework.data.domain.Pageable;
 
 public interface LibraryRuleService {
 
@@ -16,4 +19,6 @@ public interface LibraryRuleService {
     LibraryRuleResponse update(Long id, UpdateLibraryRuleRequest request);
 
     LibraryRuleResponse changeStatus(Long id, ChangeLibraryRuleStatusRequest request);
+
+    PageResponse<LibraryRuleResponse> search(Long branchId, LibraryRuleStatus status, Pageable pageable);
 }
