64f0d868611cbf6f9aa231e76bb8b5d334da7091
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 18:08:14 +0300

    update BranchService

--- backend/src/main/java/me/ifmo/backend/services/BranchService.java
+++ backend/src/main/java/me/ifmo/backend/services/BranchService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.library.request.CreateBranchRequest;
+import me.ifmo.backend.dto.library.response.BranchResponse;
+
 public interface BranchService {
+
+    BranchResponse create(CreateBranchRequest request);
 }
