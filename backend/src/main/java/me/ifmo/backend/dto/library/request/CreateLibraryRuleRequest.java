0d370a9597e218e9e0ea1db96960f2794aee7f86
misticslippers <eternal.martyr13579@gmail.com>
2026-06-25 19:02:33 +0300

    update CreateLibraryRuleRequest

--- backend/src/main/java/me/ifmo/backend/dto/library/request/CreateLibraryRuleRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/library/request/CreateLibraryRuleRequest.java
@@ -31,7 +31,6 @@ public record CreateLibraryRuleRequest(
         Integer renewalPeriodDays,
 
         Boolean reservationAllowed,
-        LocalDateTime validFrom,
         LocalDateTime validTo
 ) {
 }
