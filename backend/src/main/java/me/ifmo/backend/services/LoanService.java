a707b345e4e1f1786f24d0214f71779332e845f2
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 16:11:10 +0300

    update LoanService

--- backend/src/main/java/me/ifmo/backend/services/LoanService.java
+++ backend/src/main/java/me/ifmo/backend/services/LoanService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.circulation.request.CreateLoanRequest;
+import me.ifmo.backend.dto.circulation.response.LoanResponse;
+
 public interface LoanService {
+
+    LoanResponse create(CreateLoanRequest request);
 }
