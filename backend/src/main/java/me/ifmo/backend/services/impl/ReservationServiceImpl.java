9c370588657fce03972e1f52e080419147737a09
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 15:55:45 +0300

    update ReservationServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/ReservationServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/ReservationServiceImpl.java
@@ -1,4 +1,12 @@
 package me.ifmo.backend.services.impl;
 
-public class ReservationServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.services.ReservationService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationServiceImpl implements ReservationService {
+
+
 }
