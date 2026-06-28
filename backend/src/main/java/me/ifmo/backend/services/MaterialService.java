35939dfe945aff2cf8216852b3a588d99700f285
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 14:34:34 +0300

    update MaterialService

--- backend/src/main/java/me/ifmo/backend/services/MaterialService.java
+++ backend/src/main/java/me/ifmo/backend/services/MaterialService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.catalog.request.CreateMaterialRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialResponse;
+
 public interface MaterialService {
+
+    MaterialResponse create(CreateMaterialRequest request);
 }
