c6633c9667c54209ec0ef0cbc9627fc39863cea7
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 15:49:10 +0300

    update ReservationService

--- backend/src/main/java/me/ifmo/backend/services/ReservationService.java
+++ backend/src/main/java/me/ifmo/backend/services/ReservationService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.circulation.request.CreateReservationRequest;
+import me.ifmo.backend.dto.circulation.response.ReservationResponse;
+
 public interface ReservationService {
+
+    ReservationResponse create(CreateReservationRequest request);
 }
