f5ccd01a77697b1227d3fb2fbd6157fc0d2b2162
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 14:30:26 +0300

    update UpdateMaterialCopyRequest

--- backend/src/main/java/me/ifmo/backend/dto/catalog/request/UpdateMaterialCopyRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/catalog/request/UpdateMaterialCopyRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Size;
 import me.ifmo.backend.entities.enums.CopyStatus;
 
 public record UpdateMaterialCopyRequest(
-        CopyStatus status,
 
         @Size(max = 100)
         String shelfLocation
