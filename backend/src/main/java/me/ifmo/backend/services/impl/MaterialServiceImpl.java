a356568cb7ce4994eef6675628aa07e2a4393ab2
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 15:15:11 +0300

    update MaterialServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/MaterialServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/MaterialServiceImpl.java
@@ -1,4 +1,40 @@
 package me.ifmo.backend.services.impl;
 
-public class MaterialServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.catalog.request.CreateMaterialRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialResponse;
+import me.ifmo.backend.entities.Material;
+import me.ifmo.backend.exceptions.domain.BusinessRuleException;
+import me.ifmo.backend.exceptions.domain.DuplicateResourceException;
+import me.ifmo.backend.mappers.MaterialMapper;
+import me.ifmo.backend.repositories.MaterialRepository;
+import me.ifmo.backend.services.MaterialService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MaterialServiceImpl implements MaterialService {
+
+    private final MaterialRepository repository;
+    private final MaterialMapper mapper;
+
+    private String normalize(String value, String fieldName){
+        String normalized;
+
+        if(fieldName.equals("Title")){
+            normalized = value.strip();
+
+            if (normalized.isBlank())
+                throw new BusinessRuleException("%s must not be blank".formatted(fieldName));
+
+            return normalized;
+        } else {
+            if(value == null)
+                return null;
+
+            normalized = value.strip();
+            return normalized.isBlank() ? null : normalized;
+        }
+    }
 }
