5446786123ce79249bf76fb9d5892296cada1abe
misticslippers <eternal.martyr13579@gmail.com>
2026-06-28 16:15:49 +0300

    update LoanResponse

--- backend/src/main/java/me/ifmo/backend/dto/circulation/response/LoanResponse.java
+++ backend/src/main/java/me/ifmo/backend/dto/circulation/response/LoanResponse.java
@@ -17,6 +17,7 @@ public record LoanResponse(
         LocalDateTime loanedAt,
         LocalDateTime dueAt,
         LocalDateTime returnedAt,
+        Integer renewalCount,
         LoanStatus status
 ) {
 }
