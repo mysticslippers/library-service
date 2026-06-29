7a391517a454a28fe6054505560c770d75cee583
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:03:38 +0300

    feat: add search method FineService

--- backend/src/main/java/me/ifmo/backend/services/FineService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineService.java
@@ -1,8 +1,11 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.fine.request.CancelFineRequest;
 import me.ifmo.backend.dto.fine.request.CreateFineRequest;
+import me.ifmo.backend.dto.fine.request.FineSearchRequest;
 import me.ifmo.backend.dto.fine.response.FineResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface FineService {
 
@@ -13,4 +16,6 @@ public interface FineService {
     FineResponse cancel(Long id, CancelFineRequest request);
 
     FineResponse markPaid(Long id);
+
+    PageResponse<FineResponse> search(FineSearchRequest request, Pageable pageable);
 }
