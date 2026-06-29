72040a4716f3277c9b5f7bbea04e698f01979127
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:01:26 +0300

    feat: add search method FineRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
@@ -6,7 +6,10 @@ import me.ifmo.backend.entities.enums.ViolationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -26,4 +29,23 @@ public interface FineRepository extends JpaRepository<Fine, Long> {
     Page<Fine> findByReasonAndStatus(ViolationType reason, FineStatus status, Pageable pageable);
 
     Long countByUser_IdAndStatus(Long userId, FineStatus status);
+
+    @Query("""
+       SELECT fine FROM Fine fine
+           WHERE (:userId IS NULL OR fine.user.id = :userId)
+             AND (:loanId IS NULL OR fine.loan.id = :loanId)
+             AND (:copyId IS NULL OR fine.copy.id = :copyId)
+             AND (:reason IS NULL OR fine.reason = :reason)
+             AND (:status IS NULL OR fine.status = :status)
+             AND (:createdFrom IS NULL OR fine.createdAt >= :createdFrom)
+             AND (:createdTo IS NULL OR fine.createdAt <= :createdTo)
+    """)
+    Page<Fine> search(@Param("userId") Long userId,
+                      @Param("loanId") Long loanId,
+                      @Param("copyId") Long copyId,
+                      @Param("reason") ViolationType reason,
+                      @Param("status") FineStatus status,
+                      @Param("createdFrom") LocalDateTime createdFrom,
+                      @Param("createdTo") LocalDateTime createdTo,
+                      Pageable pageable);
 }
