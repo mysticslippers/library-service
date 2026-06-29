03ae6f44f2d78a938cecb38c491d6191ef4302e9
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:02:49 +0300

    feat: add cancel method FineService

--- backend/src/main/java/me/ifmo/backend/services/FineService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineService.java
@@ -1,5 +1,6 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.fine.request.CancelFineRequest;
 import me.ifmo.backend.dto.fine.request.CreateFineRequest;
 import me.ifmo.backend.dto.fine.response.FineResponse;
 
@@ -8,4 +9,6 @@ public interface FineService {
     FineResponse create(CreateFineRequest request);
 
     FineResponse getFineById(Long id);
+
+    FineResponse cancel(Long id, CancelFineRequest request);
 }
