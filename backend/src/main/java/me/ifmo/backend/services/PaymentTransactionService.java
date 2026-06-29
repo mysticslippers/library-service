213b37ccf89a22b8ce6b92a83d4f9a45cb40ed5f
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:39:26 +0300

    feat: add updateStatus method PaymentTransactionService

--- backend/src/main/java/me/ifmo/backend/services/PaymentTransactionService.java
+++ backend/src/main/java/me/ifmo/backend/services/PaymentTransactionService.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.fine.request.CreatePaymentTransactionRequest;
+import me.ifmo.backend.dto.fine.request.UpdatePaymentStatusRequest;
 import me.ifmo.backend.dto.fine.response.PaymentTransactionResponse;
 
 public interface PaymentTransactionService {
@@ -10,4 +11,6 @@ public interface PaymentTransactionService {
     PaymentTransactionResponse getPaymentTransactionById(Long id);
 
     PaymentTransactionResponse getByExternalPayment(String externalPayment);
+
+    PaymentTransactionResponse updateStatus(Long id, UpdatePaymentStatusRequest request);
 }
