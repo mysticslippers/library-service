534f3d374c8a3341358e9a5f6ae962f905a79cea
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:48:31 +0300

    feat: add search method

--- backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
@@ -1,10 +1,13 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.fine.request.ChangeFineTariffStatusRequest;
 import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
 import me.ifmo.backend.dto.fine.request.UpdateFineTariffRequest;
 import me.ifmo.backend.dto.fine.response.FineTariffResponse;
+import me.ifmo.backend.entities.enums.FineTariffStatus;
 import me.ifmo.backend.entities.enums.ViolationType;
+import org.springframework.data.domain.Pageable;
 
 public interface FineTariffService {
 
@@ -17,4 +20,6 @@ public interface FineTariffService {
     FineTariffResponse update(Long id, UpdateFineTariffRequest request);
 
     FineTariffResponse changeStatus(Long id, ChangeFineTariffStatusRequest request);
+
+    PageResponse<FineTariffResponse> search(ViolationType violationType, FineTariffStatus status, Pageable pageable);
 }
