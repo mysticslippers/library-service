b9dba6807ae7144bf8ebfc35270288b389454b12
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:36:57 +0300

    feat: add search method PaymentTransactionRepository

--- backend/src/main/java/me/ifmo/backend/repositories/PaymentTransactionRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/PaymentTransactionRepository.java
@@ -4,7 +4,9 @@ import me.ifmo.backend.entities.PaymentTransaction;
 import me.ifmo.backend.entities.enums.PaymentStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -25,4 +27,17 @@ public interface PaymentTransactionRepository extends CrudRepository<PaymentTran
     Page<PaymentTransaction> findByStatus(PaymentStatus status, Pageable pageable);
 
     Page<PaymentTransaction> findByStatusInAndCreatedAtBefore(Collection<PaymentStatus> statuses, LocalDateTime createdAt, Pageable pageable);
+
+    @Query("""
+       SELECT transaction FROM PaymentTransaction transaction
+           WHERE (:fineId IS NULL OR transaction.fine.id = :fineId)
+             AND (:status IS NULL OR transaction.status = :status)
+             AND (:createdFrom IS NULL OR transaction.createdAt >= :createdFrom)
+             AND (:createdTo IS NULL OR transaction.createdAt <= :createdTo)
+    """)
+    Page<PaymentTransaction> search(@Param("fineId") Long fineId,
+                                    @Param("status") PaymentStatus status,
+                                    @Param("createdFrom") LocalDateTime createdFrom,
+                                    @Param("createdTo") LocalDateTime createdTo,
+                                    Pageable pageable);
 }
