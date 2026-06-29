b476667a1ba0a5b007bf9864e7382fd14972070c
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:51:06 +0300

    feat: add create method

--- backend/src/main/java/me/ifmo/backend/services/impl/FineTariffServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/FineTariffServiceImpl.java
@@ -1,16 +1,23 @@
 package me.ifmo.backend.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
+import me.ifmo.backend.dto.fine.response.FineTariffResponse;
+import me.ifmo.backend.entities.FineTariff;
+import me.ifmo.backend.entities.enums.FineTariffStatus;
 import me.ifmo.backend.exceptions.domain.BusinessRuleException;
 import me.ifmo.backend.mappers.FineTariffMapper;
 import me.ifmo.backend.repositories.FineTariffRepository;
+import me.ifmo.backend.services.FineTariffService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class FineTariffServiceImpl {
+public class FineTariffServiceImpl implements FineTariffService {
 
     private final FineTariffRepository repository;
     private final FineTariffMapper mapper;
@@ -27,5 +34,25 @@ public class FineTariffServiceImpl {
 
         if (maxAmount != null && fixedAmount != null && maxAmount.compareTo(fixedAmount) < 0)
             throw new BusinessRuleException("Fine tariff maxAmount must not be less than fixedAmount");
+    }
+
+    @Override
+    @Transactional
+    public FineTariffResponse create(CreateFineTariffRequest request) {
+        validate(request.amountPerDay(), request.fixedAmount(), request.maxAmount());
+
+        LocalDateTime now = LocalDateTime.now();
+
+        repository.findByViolationTypeAndStatus(request.violationType(), FineTariffStatus.ACTIVE).ifPresent(activeTariff -> {
+                    activeTariff.setStatus(FineTariffStatus.INACTIVE);
+                    activeTariff.setValidTo(now);
+                });
+
+        FineTariff tariff = mapper.toEntity(request);
+        tariff.setStatus(FineTariffStatus.ACTIVE);
+        tariff.setValidTo(request.validTo());
+
+        FineTariff saved = repository.save(tariff);
+        return mapper.toResponse(saved);
     }
 }
