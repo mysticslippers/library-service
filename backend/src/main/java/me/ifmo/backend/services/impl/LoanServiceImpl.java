c5331ed1242533e37ad7b9bdbe5633901ca9ea14
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:29:17 +0300

    update LoanServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/LoanServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/LoanServiceImpl.java
@@ -1,4 +1,21 @@
 package me.ifmo.backend.services.impl;
 
-public class LoanServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.services.LoanService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoanServiceImpl implements LoanService {
+
+    private static final Set<LoanStatus> BLOCKING_LOAN_STATUSES =
+            Set.of(LoanStatus.ACTIVE, LoanStatus.OVERDUE, LoanStatus.LOST);
+
+    private final LoanRepository repository;
+    private final UserRepository userRepository;
+    private final MaterialCopyRepository materialCopyRepository;
+    private final BranchRepository branchRepository;
+    private final ReservationRepository reservationRepository;
+    private final LibraryRuleRepository libraryRuleRepository;
+    private final LoanMapper mapper;
 }
