493d48baab663311901dbfca3eadb318bcd62e27
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:40:55 +0300

    feat: add PaymentTransactionServiceImpl

---
+++ backend/src/main/java/me/ifmo/backend/services/impl/PaymentTransactionServiceImpl.java
@@ -0,0 +1,24 @@
+package me.ifmo.backend.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.entities.enums.PaymentStatus;
+import me.ifmo.backend.mappers.PaymentTransactionMapper;
+import me.ifmo.backend.repositories.FineRepository;
+import me.ifmo.backend.repositories.PaymentTransactionRepository;
+import me.ifmo.backend.services.PaymentTransactionService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentTransactionServiceImpl implements PaymentTransactionService {
+
+    private static final Set<PaymentStatus> FINAL_STATUSES =
+            Set.of(PaymentStatus.SUCCESS, PaymentStatus.DECLINED, PaymentStatus.CANCELLED,
+                    PaymentStatus.FAILED, PaymentStatus.TIMEOUT);
+
+    private final PaymentTransactionRepository repository;
+    private final FineRepository fineRepository;
+    private final PaymentTransactionMapper mapper;
+}
