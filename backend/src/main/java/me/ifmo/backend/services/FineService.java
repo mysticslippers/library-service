c5009d93cd81032a81e2804ffd769a0ea2e95389
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:02:09 +0300

    feat: add create method FineService

--- backend/src/main/java/me/ifmo/backend/services/FineService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.fine.request.CreateFineRequest;
+import me.ifmo.backend.dto.fine.response.FineResponse;
+
 public interface FineService {
+
+    FineResponse create(CreateFineRequest request);
 }
