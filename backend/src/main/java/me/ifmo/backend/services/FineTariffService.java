b62990d1778a5b2de6f77ef4e58a1523cf1ca8a0
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:47:42 +0300

    feat: add update method

--- backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
+import me.ifmo.backend.dto.fine.request.UpdateFineTariffRequest;
 import me.ifmo.backend.dto.fine.response.FineTariffResponse;
 import me.ifmo.backend.entities.enums.ViolationType;
 
@@ -11,4 +12,6 @@ public interface FineTariffService {
     FineTariffResponse getFineTariffById(Long id);
 
     FineTariffResponse getActualByViolationType(ViolationType violationType);
+
+    FineTariffResponse update(Long id, UpdateFineTariffRequest request);
 }
