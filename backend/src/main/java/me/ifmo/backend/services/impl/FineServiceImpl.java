cd6d3d027284573fdf61ad617a99b6650534dbfc
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 18:04:55 +0300

    feat: add normalize method FineServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/FineServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/FineServiceImpl.java
@@ -1,4 +1,27 @@
 package me.ifmo.backend.services.impl;
 
-public class FineServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.exceptions.domain.BusinessRuleException;
+import me.ifmo.backend.mappers.FineMapper;
+import me.ifmo.backend.repositories.*;
+import me.ifmo.backend.services.FineService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FineServiceImpl implements FineService {
+
+    private final FineRepository repository;
+    private final UserRepository userRepository;
+    private final LoanRepository loanRepository;
+    private final MaterialCopyRepository materialCopyRepository;
+    private final FineTariffRepository fineTariffRepository;
+    private final FineMapper mapper;
+
+    private String normalize(String value, String fieldName) {
+        if (value == null || value.strip().isBlank())
+            throw new BusinessRuleException("%s must not be blank".formatted(fieldName));
+
+        return value.strip();
+    }
 }
