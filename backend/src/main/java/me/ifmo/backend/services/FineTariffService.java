75868a9a01e0c6abc28babe0e6a1e214adeef196
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:47:16 +0300

    feat: add getActualByViolationType method

--- backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
@@ -2,10 +2,13 @@ package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
 import me.ifmo.backend.dto.fine.response.FineTariffResponse;
+import me.ifmo.backend.entities.enums.ViolationType;
 
 public interface FineTariffService {
 
     FineTariffResponse create(CreateFineTariffRequest request);
 
     FineTariffResponse getFineTariffById(Long id);
+
+    FineTariffResponse getActualByViolationType(ViolationType violationType);
 }
