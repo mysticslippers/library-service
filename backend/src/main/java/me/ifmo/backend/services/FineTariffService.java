9de651802e4c99f2b104c25a793186ca63d648d6
misticslippers <eternal.martyr13579@gmail.com>
2026-06-29 17:46:37 +0300

    feat: add create method

--- backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
+++ backend/src/main/java/me/ifmo/backend/services/FineTariffService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
+import me.ifmo.backend.dto.fine.response.FineTariffResponse;
+
 public interface FineTariffService {
+
+    FineTariffResponse create(CreateFineTariffRequest request);
 }
