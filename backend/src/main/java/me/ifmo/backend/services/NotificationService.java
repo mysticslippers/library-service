3868ff4410405185bcff6060eab52bd3197175d9
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:08:11 +0300

    feat: add search NotificationService

--- backend/src/main/java/me/ifmo/backend/services/NotificationService.java
+++ backend/src/main/java/me/ifmo/backend/services/NotificationService.java
@@ -1,8 +1,11 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.notification.request.CreateNotificationRequest;
+import me.ifmo.backend.dto.notification.request.NotificationSearchRequest;
 import me.ifmo.backend.dto.notification.request.UpdateNotificationStatusRequest;
 import me.ifmo.backend.dto.notification.response.NotificationResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface NotificationService {
 
@@ -11,4 +14,6 @@ public interface NotificationService {
     NotificationResponse getNotificationById(Long id);
 
     NotificationResponse updateStatus(Long id, UpdateNotificationStatusRequest request);
+
+    PageResponse<NotificationResponse> search(NotificationSearchRequest request, Pageable pageable);
 }
