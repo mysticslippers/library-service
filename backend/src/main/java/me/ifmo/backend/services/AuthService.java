d76203e6d64e9e1fbd59f792e908d9944fd431af
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:04:24 +0300

    feat: add me method AuthService

--- backend/src/main/java/me/ifmo/backend/services/AuthService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuthService.java
@@ -3,10 +3,13 @@ package me.ifmo.backend.services;
 import me.ifmo.backend.dto.auth.request.LoginRequest;
 import me.ifmo.backend.dto.auth.request.RegisterRequest;
 import me.ifmo.backend.dto.auth.response.AuthResponse;
+import me.ifmo.backend.dto.user.response.UserProfileResponse;
 
 public interface AuthService {
 
     AuthResponse register(RegisterRequest request);
 
     AuthResponse login(LoginRequest request);
+
+    UserProfileResponse me(Long userId);
 }
