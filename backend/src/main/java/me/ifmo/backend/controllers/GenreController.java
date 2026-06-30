b23a75edfa1e4f697f5d94e47be9b7f08784ea29
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:19:47 +0300

    feat: add GenreController

---
+++ backend/src/main/java/me/ifmo/backend/controllers/GenreController.java
@@ -0,0 +1,11 @@
+package me.ifmo.backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genres")
+@RequiredArgsConstructor
+public class GenreController {
+}
