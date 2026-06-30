04c490eae3bbae5db12e7755446e8bc42224b162
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:01:54 +0300

    feat: add generateAccessToken method JwtService

--- backend/src/main/java/me/ifmo/backend/services/JwtService.java
+++ backend/src/main/java/me/ifmo/backend/services/JwtService.java
@@ -1,4 +1,11 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.entities.User;
+import me.ifmo.backend.entities.enums.RoleCode;
+
+import java.util.Collection;
+
 public interface JwtService {
+
+    String generateAccessToken(User user, Collection<RoleCode> roles);
 }
