a3fd759928562d8ddb330439815d3ee097051067
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:06:23 +0300

    feat: add normalize method AuthServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/AuthServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/AuthServiceImpl.java
@@ -1,4 +1,44 @@
 package me.ifmo.backend.services.impl;
 
-public class AuthServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.exceptions.domain.BusinessRuleException;
+import me.ifmo.backend.mappers.UserMapper;
+import me.ifmo.backend.repositories.RoleRepository;
+import me.ifmo.backend.repositories.UserRepository;
+import me.ifmo.backend.repositories.UserRoleRepository;
+import me.ifmo.backend.services.AuthService;
+import me.ifmo.backend.services.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private static final short MAX_FAILED_ATTEMPTS = 5;
+    private static final long LOCK_MINUTES = 15;
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    private String normalize(String value, String fieldName) {
+        if(fieldName.equals("Middle name")){
+            if (value == null || value.strip().isBlank())
+                return null;
+
+        } else {
+            if(fieldName.equals("Email"))
+                value = value.toLowerCase(Locale.ROOT);
+
+            if (value == null || value.strip().isBlank())
+                throw new BusinessRuleException("%s must not be blank".formatted(fieldName));
+        }
+        return value.strip();
+    }
 }
