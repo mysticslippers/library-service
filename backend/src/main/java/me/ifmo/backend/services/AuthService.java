24b25b41e35bd8c009cf5f6514871ec0f2f956b6
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:02:51 +0300

    feat: add register method AuthService

--- backend/src/main/java/me/ifmo/backend/services/AuthService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuthService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.auth.request.RegisterRequest;
+import me.ifmo.backend.dto.auth.response.AuthResponse;
+
 public interface AuthService {
+
+    AuthResponse register(RegisterRequest request);
 }
