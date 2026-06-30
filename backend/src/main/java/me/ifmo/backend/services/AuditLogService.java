776b60956a0c5bd704734754111d775350f32da3
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:24:14 +0300

    feat: add search method AuditLogService

--- backend/src/main/java/me/ifmo/backend/services/AuditLogService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuditLogService.java
@@ -1,8 +1,11 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.audit.request.AuditLogSearchRequest;
 import me.ifmo.backend.dto.audit.response.AuditLogResponse;
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.entities.enums.AuditAction;
 import me.ifmo.backend.entities.enums.AuditEntityType;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Map;
 
@@ -12,4 +15,6 @@ public interface AuditLogService {
                             AuditAction action, Map<String, Object> details);
 
     AuditLogResponse getAuditLogById(Long id);
+
+    PageResponse<AuditLogResponse> search(AuditLogSearchRequest request, Pageable pageable);
 }
