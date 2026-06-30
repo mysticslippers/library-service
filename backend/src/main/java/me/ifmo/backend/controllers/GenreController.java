24ab67b869fb851f416bab27ccaba7f362edbba6
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:20:31 +0300

    feat: add create method GenreController

--- backend/src/main/java/me/ifmo/backend/controllers/GenreController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/GenreController.java
@@ -1,11 +1,23 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.ifmo.backend.dto.catalog.request.CreateGenreRequest;
+import me.ifmo.backend.dto.catalog.response.GenreResponse;
+import me.ifmo.backend.services.GenreService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/genres")
 @RequiredArgsConstructor
 public class GenreController {
+
+    private final GenreService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GenreResponse create(@Valid @RequestBody CreateGenreRequest request) {
+        return service.create(request);
+    }
 }
