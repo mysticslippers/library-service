2a6ef2b80d68fcb007e020afa5629fdec4b2583a
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:40:58 +0300

    feat: add UserServiceImpl

---
+++ backend/src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package me.ifmo.backend.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+}
