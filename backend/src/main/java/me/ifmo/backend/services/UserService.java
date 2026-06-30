de44e52ba0db1e3b4291d38402fb76bf87284e26
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:37:43 +0300

    feat: add getProfile method UserService

--- backend/src/main/java/me/ifmo/backend/services/UserService.java
+++ backend/src/main/java/me/ifmo/backend/services/UserService.java
@@ -2,10 +2,13 @@ package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.user.request.CreateUserRequest;
 import me.ifmo.backend.dto.user.response.UserAdminResponse;
+import me.ifmo.backend.dto.user.response.UserProfileResponse;
 
 public interface UserService {
 
     UserAdminResponse create(CreateUserRequest request);
 
     UserAdminResponse getUserById(Long id);
+
+    UserProfileResponse getProfile(Long id);
 }
