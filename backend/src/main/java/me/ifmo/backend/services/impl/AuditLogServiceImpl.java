8c7a413cbdc75289e3589fc80853f5fb20efcf7b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:25:11 +0300

    feat: update AuditLogServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/AuditLogServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/AuditLogServiceImpl.java
@@ -1,4 +1,13 @@
 package me.ifmo.backend.services.impl;
 
-public class AuditLogServiceImpl {
+import me.ifmo.backend.mappers.AuditLogMapper;
+import me.ifmo.backend.repositories.AuditLogRepository;
+import me.ifmo.backend.repositories.UserRepository;
+import me.ifmo.backend.services.AuditLogService;
+
+public class AuditLogServiceImpl implements AuditLogService {
+
+    private final AuditLogRepository repository;
+    private final UserRepository userRepository;
+    private final AuditLogMapper mapper;
 }
