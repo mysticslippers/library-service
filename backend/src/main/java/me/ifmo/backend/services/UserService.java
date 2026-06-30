ac85d07c562ad7c6a3fef5cf93e4cb296b473cbc
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:36:57 +0300

    feat: add create method UserService

--- backend/src/main/java/me/ifmo/backend/services/UserService.java
+++ backend/src/main/java/me/ifmo/backend/services/UserService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.user.request.CreateUserRequest;
+import me.ifmo.backend.dto.user.response.UserAdminResponse;
+
 public interface UserService {
+
+    UserAdminResponse create(CreateUserRequest request);
 }
