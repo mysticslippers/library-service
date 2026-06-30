3e949f872a38a3febb98c74b4cb38c580ba2f14b
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 18:03:12 +0300

    feat: add login method AuthService

--- backend/src/main/java/me/ifmo/backend/services/AuthService.java
+++ backend/src/main/java/me/ifmo/backend/services/AuthService.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.auth.request.LoginRequest;
 import me.ifmo.backend.dto.auth.request.RegisterRequest;
 import me.ifmo.backend.dto.auth.response.AuthResponse;
 
 public interface AuthService {
 
     AuthResponse register(RegisterRequest request);
+
+    AuthResponse login(LoginRequest request);
 }
