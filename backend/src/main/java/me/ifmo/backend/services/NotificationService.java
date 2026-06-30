56c40f5ab1755d96961ca31e33c16a3d30a41bd7
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:07:45 +0300

    feat: add updateStatus NotificationService

--- backend/src/main/java/me/ifmo/backend/services/NotificationService.java
+++ backend/src/main/java/me/ifmo/backend/services/NotificationService.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.services;
 
 import me.ifmo.backend.dto.notification.request.CreateNotificationRequest;
+import me.ifmo.backend.dto.notification.request.UpdateNotificationStatusRequest;
 import me.ifmo.backend.dto.notification.response.NotificationResponse;
 
 public interface NotificationService {
@@ -8,4 +9,6 @@ public interface NotificationService {
     NotificationResponse create(CreateNotificationRequest request);
 
     NotificationResponse getNotificationById(Long id);
+
+    NotificationResponse updateStatus(Long id, UpdateNotificationStatusRequest request);
 }
