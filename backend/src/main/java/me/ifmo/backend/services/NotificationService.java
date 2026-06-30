015d0502a89807a962b9801a2daa1a17b0a438b4
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:07:05 +0300

    feat: add create NotificationService

--- backend/src/main/java/me/ifmo/backend/services/NotificationService.java
+++ backend/src/main/java/me/ifmo/backend/services/NotificationService.java
@@ -1,4 +1,9 @@
 package me.ifmo.backend.services;
 
+import me.ifmo.backend.dto.notification.request.CreateNotificationRequest;
+import me.ifmo.backend.dto.notification.response.NotificationResponse;
+
 public interface NotificationService {
+
+    NotificationResponse create(CreateNotificationRequest request);
 }
