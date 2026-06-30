0ac23f1173eeb2b0ac5a8430e2e6889edc238391
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:46:08 +0300

    feat: add normalize method UserServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package me.ifmo.backend.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.exceptions.domain.BusinessRuleException;
 import me.ifmo.backend.mappers.UserMapper;
 import me.ifmo.backend.repositories.BranchRepository;
 import me.ifmo.backend.repositories.RoleRepository;
@@ -9,6 +10,8 @@ import me.ifmo.backend.repositories.UserRoleRepository;
 import me.ifmo.backend.services.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +23,19 @@ public class UserServiceImpl implements UserService {
     private final UserRoleRepository userRoleRepository;
     private final UserMapper mapper;
     private final PasswordEncoder passwordEncoder;
+
+    private String normalize(String value, String fieldName) {
+        if(fieldName.equals("Middle name")){
+            if (value == null || value.strip().isBlank())
+                return null;
+
+        } else {
+            if(fieldName.equals("Email"))
+                value = value.toLowerCase(Locale.ROOT);
+
+            if (value == null || value.strip().isBlank())
+                throw new BusinessRuleException("%s must not be blank".formatted(fieldName));
+        }
+        return value.strip();
+    }
 }
