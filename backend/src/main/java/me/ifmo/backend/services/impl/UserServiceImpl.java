85be32858633e709f6a436f0ac4d5aaab0632e2a
misticslippers <eternal.martyr13579@gmail.com>
2026-06-30 17:41:40 +0300

    feat: update UserServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
@@ -1,10 +1,23 @@
 package me.ifmo.backend.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.mappers.UserMapper;
+import me.ifmo.backend.repositories.BranchRepository;
+import me.ifmo.backend.repositories.RoleRepository;
+import me.ifmo.backend.repositories.UserRepository;
+import me.ifmo.backend.repositories.UserRoleRepository;
 import me.ifmo.backend.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+    private final BranchRepository branchRepository;
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final UserMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 }
