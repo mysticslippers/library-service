e5738da74f2056c840281e1e5e30d05d13f3a19e
misticslippers <eternal.martyr13579@gmail.com>
2026-07-01 19:25:51 +0300

    feat: add loadUserByUsername method CustomUserDetailsService

--- backend/src/main/java/me/ifmo/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/me/ifmo/backend/security/CustomUserDetailsService.java
@@ -1,4 +1,41 @@
 package me.ifmo.backend.security;
 
-public class CustomUserDetailsService {
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.entities.User;
+import me.ifmo.backend.entities.enums.RoleCode;
+import me.ifmo.backend.repositories.UserRepository;
+import me.ifmo.backend.repositories.UserRoleRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(
+                () -> new UsernameNotFoundException("User not found"));
+
+        List<SimpleGrantedAuthority> authorities = userRoleRepository.findRoleCodesByUser_Id(user.getId()).stream()
+                .map(RoleCode::name)
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        return org.springframework.security.core.userdetails.User.withUsername(user.getId().toString())
+                .password(user.getPasswordHash())
+                .authorities(authorities)
+                .disabled(user.getStatus().name().equals("ARCHIVED"))
+                .accountLocked(user.getStatus().name().equals("BLOCKED"))
+                .build();
+    }
 }
