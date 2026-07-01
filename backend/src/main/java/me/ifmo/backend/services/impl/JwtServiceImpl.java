d09a5de85c3643daa710e28ebafb71ea6a51154f
misticslippers <eternal.martyr13579@gmail.com>
2026-07-01 19:17:44 +0300

    feat: add generateAccessToken method JwtServiceImpl

--- backend/src/main/java/me/ifmo/backend/services/impl/JwtServiceImpl.java
+++ backend/src/main/java/me/ifmo/backend/services/impl/JwtServiceImpl.java
@@ -1,12 +1,19 @@
 package me.ifmo.backend.services.impl;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Value;
+import me.ifmo.backend.entities.User;
+import me.ifmo.backend.entities.enums.RoleCode;
 import me.ifmo.backend.services.JwtService;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class JwtServiceImpl  implements JwtService {
@@ -17,5 +24,20 @@ public class JwtServiceImpl  implements JwtService {
     public JwtServiceImpl(@Value("${security.jwt.secret}") String secret, @Value("${security.jwt.access-token-expiration-ms}") long accessTokenExpiresIn) {
         this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
         this.accessTokenExpiresIn = accessTokenExpiresIn;
+    }
+
+    @Override
+    public String generateAccessToken(User user, Collection<RoleCode> roles) {
+        Instant now = Instant.now();
+
+        List<String> roleNames = roles.stream().map(RoleCode::name).toList();
+
+        return Jwts.builder().subject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("roles", roleNames)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusMillis(accessTokenExpiresIn)))
+                .signWith(key)
+                .compact();
     }
 }
