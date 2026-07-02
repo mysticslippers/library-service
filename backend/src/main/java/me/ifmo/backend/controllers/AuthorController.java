3b3c4cd7acd0ec53809727cb909cfa4db0098811
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:07:48 +0300

    feat: add create method AuthorController

--- backend/src/main/java/me/ifmo/backend/controllers/AuthorController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/AuthorController.java
@@ -1,4 +1,23 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.catalog.request.CreateAuthorRequest;
+import me.ifmo.backend.dto.catalog.response.AuthorResponse;
+import me.ifmo.backend.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/authors")
+@RequiredArgsConstructor
 public class AuthorController {
+
+    private final AuthorService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthorResponse create(@Valid @RequestBody CreateAuthorRequest request) {
+        return service.create(request);
+    }
 }
