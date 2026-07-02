aff62e97ecb2ab8a4292aac87b7223f1cc51c003
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:14:40 +0300

    feat: add create method MaterialCopyController

--- backend/src/main/java/me/ifmo/backend/controllers/MaterialCopyController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/MaterialCopyController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.catalog.request.CreateMaterialCopyRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialCopyResponse;
 import me.ifmo.backend.services.MaterialCopyService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/material-copies")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialCopyController {
 
     private final MaterialCopyService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MaterialCopyResponse create(@Valid @RequestBody CreateMaterialCopyRequest request) {
+        return service.create(request);
+    }
 }
