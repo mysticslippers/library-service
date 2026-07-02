9594d4a2d5987b31a7dc5a144a0b6336bd51d3cd
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:21:31 +0300

    feat: add create method LibraryController

--- backend/src/main/java/me/ifmo/backend/controllers/LibraryController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/LibraryController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.library.request.CreateLibraryRequest;
+import me.ifmo.backend.dto.library.response.LibraryResponse;
 import me.ifmo.backend.services.LibraryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/libraries")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryController {
 
     private final LibraryService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public LibraryResponse create(@Valid @RequestBody CreateLibraryRequest request) {
+        return service.create(request);
+    }
 }
