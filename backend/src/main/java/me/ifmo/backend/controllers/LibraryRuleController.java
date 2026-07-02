b423f35f4fae7e9d96c5a4434dcf4897b07079d2
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:27:23 +0300

    feat: add create method LibraryRuleController

--- backend/src/main/java/me/ifmo/backend/controllers/LibraryRuleController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/LibraryRuleController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
+import me.ifmo.backend.dto.library.response.LibraryRuleResponse;
 import me.ifmo.backend.services.LibraryRuleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/library-rules")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryRuleController {
 
     private final LibraryRuleService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public LibraryRuleResponse create(@Valid @RequestBody CreateLibraryRuleRequest request) {
+        return service.create(request);
+    }
 }
