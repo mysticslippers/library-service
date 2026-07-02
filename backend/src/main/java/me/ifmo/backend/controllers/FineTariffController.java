0b7d43d47056ce17a4b5f300fca5885547721db3
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:45:25 +0300

    feat: add FineTariffController

---
+++ backend/src/main/java/me/ifmo/backend/controllers/FineTariffController.java
@@ -0,0 +1,54 @@
+package me.ifmo.backend.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.common.response.PageResponse;
+import me.ifmo.backend.dto.fine.request.ChangeFineTariffStatusRequest;
+import me.ifmo.backend.dto.fine.request.CreateFineTariffRequest;
+import me.ifmo.backend.dto.fine.request.UpdateFineTariffRequest;
+import me.ifmo.backend.dto.fine.response.FineTariffResponse;
+import me.ifmo.backend.entities.enums.FineTariffStatus;
+import me.ifmo.backend.entities.enums.ViolationType;
+import me.ifmo.backend.services.FineTariffService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/fine-tariffs")
+@RequiredArgsConstructor
+public class FineTariffController {
+
+    private final FineTariffService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public FineTariffResponse create(@Valid @RequestBody CreateFineTariffRequest request) {
+        return service.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public FineTariffResponse getFineTariffById(@PathVariable Long id) {
+        return service.getFineTariffById(id);
+    }
+
+    @GetMapping("/actual/{violationType}")
+    public FineTariffResponse getActualByViolationType(@PathVariable ViolationType violationType) {
+        return service.getActualByViolationType(violationType);
+    }
+
+    @PatchMapping("/{id}")
+    public FineTariffResponse update(@PathVariable Long id, @Valid @RequestBody UpdateFineTariffRequest request) {
+        return service.update(id, request);
+    }
+
+    @PatchMapping("/{id}/status")
+    public FineTariffResponse changeStatus(@PathVariable Long id, @Valid @RequestBody ChangeFineTariffStatusRequest request) {
+        return service.changeStatus(id, request);
+    }
+
+    @GetMapping
+    public PageResponse<FineTariffResponse> search(@RequestParam(required = false) ViolationType violationType, @RequestParam(required = false) FineTariffStatus status, Pageable pageable) {
+        return service.search(violationType, status, pageable);
+    }
+}
