618e3186cf493c63f5f03919b97fa247ea75e538
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:18:22 +0300

    feat: add create method BranchController

--- backend/src/main/java/me/ifmo/backend/controllers/BranchController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/BranchController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.library.request.CreateBranchRequest;
+import me.ifmo.backend.dto.library.response.BranchResponse;
 import me.ifmo.backend.services.BranchService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/branches")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BranchController {
 
     private final BranchService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BranchResponse create(@Valid @RequestBody CreateBranchRequest request) {
+        return service.create(request);
+    }
 }
