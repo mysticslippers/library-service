c0b5b1b438567efa58a9042e55e5a1508929d069
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:29:13 +0300

    feat: add search method LibraryRuleController

--- backend/src/main/java/me/ifmo/backend/controllers/LibraryRuleController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/LibraryRuleController.java
@@ -2,11 +2,14 @@ package me.ifmo.backend.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.library.request.ChangeLibraryRuleStatusRequest;
 import me.ifmo.backend.dto.library.request.CreateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.request.UpdateLibraryRuleRequest;
 import me.ifmo.backend.dto.library.response.LibraryRuleResponse;
+import me.ifmo.backend.entities.enums.LibraryRuleStatus;
 import me.ifmo.backend.services.LibraryRuleService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,5 +44,10 @@ public class LibraryRuleController {
     @PatchMapping("/{id}/status")
     public LibraryRuleResponse changeStatus(@PathVariable Long id, @Valid @RequestBody ChangeLibraryRuleStatusRequest request) {
         return service.changeStatus(id, request);
+    }
+
+    @GetMapping
+    public PageResponse<LibraryRuleResponse> search(@RequestParam(required = false) Long branchId, @RequestParam(required = false) LibraryRuleStatus status, Pageable pageable) {
+        return service.search(branchId, status, pageable);
     }
 }
