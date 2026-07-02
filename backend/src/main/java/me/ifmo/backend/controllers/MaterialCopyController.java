8f9194012f40f582a64d3aa110abec0e307caeb9
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:16:46 +0300

    feat: add search method MaterialCopyController

--- backend/src/main/java/me/ifmo/backend/controllers/MaterialCopyController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/MaterialCopyController.java
@@ -6,7 +6,10 @@ import me.ifmo.backend.dto.catalog.request.ChangeMaterialCopyStatusRequest;
 import me.ifmo.backend.dto.catalog.request.CreateMaterialCopyRequest;
 import me.ifmo.backend.dto.catalog.request.UpdateMaterialCopyRequest;
 import me.ifmo.backend.dto.catalog.response.MaterialCopyResponse;
+import me.ifmo.backend.dto.common.response.PageResponse;
+import me.ifmo.backend.entities.enums.CopyStatus;
 import me.ifmo.backend.services.MaterialCopyService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,5 +44,10 @@ public class MaterialCopyController {
     @PatchMapping("/{id}/status")
     public MaterialCopyResponse changeStatus(@PathVariable Long id, @Valid @RequestBody ChangeMaterialCopyStatusRequest request) {
         return service.changeStatus(id, request);
+    }
+
+    @GetMapping
+    public PageResponse<MaterialCopyResponse> search(@RequestParam(required = false) Long materialId, @RequestParam(required = false) Long branchId, @RequestParam(required = false) CopyStatus status, Pageable pageable) {
+        return service.search(materialId, branchId, status, pageable);
     }
 }
