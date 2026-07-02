2ae0ac379ef063a8c26850cf3ef09b3dd54d2f7e
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:10:56 +0300

    feat: add create method MaterialController

--- backend/src/main/java/me/ifmo/backend/controllers/MaterialController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/MaterialController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.catalog.request.CreateMaterialRequest;
+import me.ifmo.backend.dto.catalog.response.MaterialResponse;
 import me.ifmo.backend.services.MaterialService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/materials")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialController {
 
     private final MaterialService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MaterialResponse create(@Valid @RequestBody CreateMaterialRequest request) {
+        return service.create(request);
+    }
 }
