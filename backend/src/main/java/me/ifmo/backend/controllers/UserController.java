23be9f15f66f0a301da2a294be260c2c865edaca
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:33:38 +0300

    feat: add create method UserController

--- backend/src/main/java/me/ifmo/backend/controllers/UserController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/UserController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.user.request.CreateUserRequest;
+import me.ifmo.backend.dto.user.response.UserAdminResponse;
 import me.ifmo.backend.services.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserAdminResponse create(@Valid @RequestBody CreateUserRequest request) {
+        return service.create(request);
+    }
 }
