72fd45fd07023cf493c28f88e3bbcd4faeeaf3a4
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 15:12:58 +0300

    feat: add RoleController

---
+++ backend/src/main/java/me/ifmo/backend/controllers/RoleController.java
@@ -0,0 +1,32 @@
+package me.ifmo.backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.common.response.PageResponse;
+import me.ifmo.backend.dto.user.response.RoleResponse;
+import me.ifmo.backend.entities.enums.RoleCode;
+import me.ifmo.backend.services.RoleService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+public class RoleController {
+
+    private final RoleService service;
+
+    @GetMapping("/{id}")
+    public RoleResponse getRoleById(@PathVariable Long id) {
+        return service.getRoleById(id);
+    }
+
+    @GetMapping("/code/{code}")
+    public RoleResponse getByCode(@PathVariable RoleCode code) {
+        return service.getByCode(code);
+    }
+
+    @GetMapping
+    public PageResponse<RoleResponse> search(Pageable pageable) {
+        return service.search(pageable);
+    }
+}
