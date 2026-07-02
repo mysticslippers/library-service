dc73c55b8adbacbd67a535a3c07c8765367a1cad
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 15:08:41 +0300

    feat: add RoleService

---
+++ backend/src/main/java/me/ifmo/backend/services/RoleService.java
@@ -0,0 +1,15 @@
+package me.ifmo.backend.services;
+
+import me.ifmo.backend.dto.common.response.PageResponse;
+import me.ifmo.backend.dto.user.response.RoleResponse;
+import me.ifmo.backend.entities.enums.RoleCode;
+import org.springframework.data.domain.Pageable;
+
+public interface RoleService {
+
+    RoleResponse getRoleById(Long id);
+
+    RoleResponse getByCode(RoleCode code);
+
+    PageResponse<RoleResponse> search(Pageable pageable);
+}
