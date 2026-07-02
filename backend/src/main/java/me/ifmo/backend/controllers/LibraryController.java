a7ab1b7f603094da28a548db25340fae3a0c8a47
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:25:41 +0300

    feat: add search method LibraryController

--- backend/src/main/java/me/ifmo/backend/controllers/LibraryController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/LibraryController.java
@@ -2,11 +2,14 @@ package me.ifmo.backend.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.dto.library.request.ChangeLibraryStatusRequest;
 import me.ifmo.backend.dto.library.request.CreateLibraryRequest;
 import me.ifmo.backend.dto.library.request.UpdateLibraryRequest;
 import me.ifmo.backend.dto.library.response.LibraryResponse;
+import me.ifmo.backend.entities.enums.LibraryStatus;
 import me.ifmo.backend.services.LibraryService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,5 +44,10 @@ public class LibraryController {
     @PatchMapping("/{id}/status")
     public LibraryResponse changeStatus(@PathVariable Long id, @Valid @RequestBody ChangeLibraryStatusRequest request) {
         return service.changeStatus(id, request);
+    }
+
+    @GetMapping
+    public PageResponse<LibraryResponse> search(@RequestParam(required = false) String query, @RequestParam(required = false) LibraryStatus status, Pageable pageable) {
+        return service.search(query, status, pageable);
     }
 }
