f2b45c2dbb9663dab1912bf8af013398b849a30e
misticslippers <eternal.martyr13579@gmail.com>
2026-07-02 14:31:03 +0300

    feat: add register method AuthController

--- backend/src/main/java/me/ifmo/backend/controllers/AuthController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.auth.request.RegisterRequest;
+import me.ifmo.backend.dto.auth.response.AuthResponse;
 import me.ifmo.backend.services.AuthService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthResponse register(@Valid @RequestBody RegisterRequest request) {
+        return service.register(request);
+    }
 }
