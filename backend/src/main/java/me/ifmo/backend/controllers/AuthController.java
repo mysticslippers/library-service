e84a270d065ce3f1c1ea333ba0d979d380b0c641
misticslippers <eternal.martyr13579@gmail.com>
2026-07-03 20:57:30 +0300

    update AuthController

--- backend/src/main/java/me/ifmo/backend/controllers/AuthController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/AuthController.java
@@ -2,8 +2,13 @@ package me.ifmo.backend.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.dto.auth.request.ActivateAccountRequest;
 import me.ifmo.backend.dto.auth.request.LoginRequest;
+import me.ifmo.backend.dto.auth.request.PasswordRecoveryRequest;
+import me.ifmo.backend.dto.auth.request.PasswordResetRequest;
 import me.ifmo.backend.dto.auth.request.RegisterRequest;
+import me.ifmo.backend.dto.auth.request.ResendActivationRequest;
+import me.ifmo.backend.dto.auth.response.AuthMessageResponse;
 import me.ifmo.backend.dto.auth.response.AuthResponse;
 import me.ifmo.backend.dto.user.response.UserProfileResponse;
 import me.ifmo.backend.services.AuthService;
@@ -21,13 +26,33 @@ public class AuthController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public AuthResponse register(@Valid @RequestBody RegisterRequest request) {
+    public AuthMessageResponse register(@Valid @RequestBody RegisterRequest request) {
         return service.register(request);
     }
 
     @PostMapping("/login")
     public AuthResponse login(@Valid @RequestBody LoginRequest request) {
         return service.login(request);
+    }
+
+    @PostMapping("/activate")
+    public AuthMessageResponse activate(@Valid @RequestBody ActivateAccountRequest request) {
+        return service.activate(request);
+    }
+
+    @PostMapping("/resend-activation")
+    public AuthMessageResponse resendActivation(@Valid @RequestBody ResendActivationRequest request) {
+        return service.resendActivation(request);
+    }
+
+    @PostMapping("/password-recovery")
+    public AuthMessageResponse requestPasswordRecovery(@Valid @RequestBody PasswordRecoveryRequest request) {
+        return service.requestPasswordRecovery(request);
+    }
+
+    @PostMapping("/password-reset")
+    public AuthMessageResponse resetPassword(@Valid @RequestBody PasswordResetRequest request) {
+        return service.resetPassword(request);
     }
 
     @GetMapping("/me")
