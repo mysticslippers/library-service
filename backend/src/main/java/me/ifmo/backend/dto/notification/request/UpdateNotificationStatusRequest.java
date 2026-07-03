cc07a7656c5a80baf385d7205b320ce19d698bcb
misticslippers <eternal.martyr13579@gmail.com>
2026-07-03 21:52:55 +0300

    update UpdateNotificationStatusRequest

--- backend/src/main/java/me/ifmo/backend/dto/notification/request/UpdateNotificationStatusRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/notification/request/UpdateNotificationStatusRequest.java
@@ -1,10 +1,17 @@
 package me.ifmo.backend.dto.notification.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import me.ifmo.backend.entities.enums.NotificationStatus;
 
 public record UpdateNotificationStatusRequest(
         @NotNull
-        NotificationStatus status
+        NotificationStatus status,
+
+        @Size(max = 255)
+        String externalMessageId,
+
+        @Size(max = 2000)
+        String errorMessage
 ) {
 }
