44157dba38b51194793b4f2b3d4db480f437c6fa
misticslippers <eternal.martyr13579@gmail.com>
2026-07-09 13:35:27 +0300

    feat: update UpdateMaterialCopyRequest

--- backend/src/main/java/me/ifmo/backend/dto/catalog/request/UpdateMaterialCopyRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/catalog/request/UpdateMaterialCopyRequest.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.dto.catalog.request;
 
 import jakarta.validation.constraints.Size;
-import me.ifmo.backend.entities.enums.CopyStatus;
 
 public record UpdateMaterialCopyRequest(
+        Long branchId,
+
+        @Size(max = 100)
+        String inventoryNumber,
 
         @Size(max = 100)
         String shelfLocation
