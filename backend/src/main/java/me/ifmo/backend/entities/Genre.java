0e946fa4d70d32f57252a19bb7f3184bdcfb0dd8
misticslippers <eternal.martyr13579@gmail.com>
2026-07-09 12:49:24 +0300

    feat: add field status Genre

--- backend/src/main/java/me/ifmo/backend/entities/Genre.java
+++ backend/src/main/java/me/ifmo/backend/entities/Genre.java
@@ -2,7 +2,10 @@ package me.ifmo.backend.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import me.ifmo.backend.entities.enums.GenreStatus;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -26,6 +29,12 @@ public class Genre {
 
     @Column(name = "name", nullable = false, unique = true, length = 100)
     private String name;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(name = "status", nullable = false, columnDefinition = "genre_status")
+    private GenreStatus status = GenreStatus.ACTIVE;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
