3ff0491d534f6eb79e767835a8428bc44f779db3
misticslippers <eternal.martyr13579@gmail.com>
2026-07-09 12:51:09 +0300

    feat: add field status GenreRepository

--- backend/src/main/java/me/ifmo/backend/repositories/GenreRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/GenreRepository.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.Genre;
+import me.ifmo.backend.entities.enums.GenreStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -17,5 +20,12 @@ public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     boolean existsByNameIgnoreCase(String name);
 
-    Page<Genre> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    @Query("""
+           SELECT genre FROM Genre genre
+               WHERE (:query IS NULL OR :query = ''
+                   OR lower(genre.code) LIKE lower(concat('%', :query, '%'))
+                   OR lower(genre.name) LIKE lower(concat('%', :query, '%')))
+                   AND genre.status = :status
+    """)
+    Page<Genre> search(@Param("query") String query, @Param("status") GenreStatus status, Pageable pageable);
 }
