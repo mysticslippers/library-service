f5c448ce5f7921212fd267f2c4c729aac82af142
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 12:54:43 +0300

    feat: update details field AuditLog

--- backend/src/main/java/me/ifmo/backend/dto/audit/request/AuditLogSearchRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/audit/request/AuditLogSearchRequest.java
@@ -1,16 +1,24 @@
 package me.ifmo.backend.dto.audit.request;
 
+import jakarta.validation.constraints.Positive;
 import me.ifmo.backend.entities.enums.AuditAction;
 import me.ifmo.backend.entities.enums.AuditEntityType;
 
 import java.time.LocalDateTime;
 
 public record AuditLogSearchRequest(
+        @Positive
         Long actorUserId,
+
         AuditEntityType type,
+
+        @Positive
         Long entityId,
+
         AuditAction action,
+
         LocalDateTime createdFrom,
+
         LocalDateTime createdTo
 ) {
 }
