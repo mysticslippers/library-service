53553a71b2943e72f0f32a48eef122625b71fcb8
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 13:04:14 +0300

    feat: delete unused methods MaterialGenreRepository

--- backend/src/main/java/me/ifmo/backend/repositories/MaterialGenreRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialGenreRepository.java
@@ -5,21 +5,11 @@ import me.ifmo.backend.entities.id.MaterialGenreId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MaterialGenreRepository extends JpaRepository<MaterialGenre, MaterialGenreId> {
 
     List<MaterialGenre> findByMaterial_Id(Long materialId);
 
-    List<MaterialGenre> findByGenre_Id(Long genreId);
-
-    boolean existsByGenre_Id(Long genreId);
-
-    Optional<MaterialGenre> findByMaterial_IdAndGenre_Id(Long materialId, Long genreId);
-
-    boolean existsByMaterial_IdAndGenre_Id(Long materialId, Long genreId);
-
     void deleteByMaterial_Id(Long materialId);
 
-    void deleteByMaterial_IdAndGenre_Id(Long materialId, Long genreId);
 }
