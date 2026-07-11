9c9fb00a6bf06e838aae6be80357b98f43eb3da2
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 13:03:15 +0300

    feat: delete unused methods MaterialAuthorRepository

--- backend/src/main/java/me/ifmo/backend/repositories/MaterialAuthorRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialAuthorRepository.java
@@ -5,21 +5,11 @@ import me.ifmo.backend.entities.id.MaterialAuthorId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MaterialAuthorRepository extends JpaRepository<MaterialAuthor, MaterialAuthorId> {
 
     List<MaterialAuthor> findByMaterial_IdOrderByAuthorOrderAsc(Long materialId);
 
-    List<MaterialAuthor> findByAuthor_Id(Long authorId);
-
-    boolean existsByAuthor_Id(Long authorId);
-
-    Optional<MaterialAuthor> findByMaterial_IdAndAuthor_Id(Long materialId, Long authorId);
-
-    boolean existsByMaterial_IdAndAuthor_Id(Long materialId, Long authorId);
-
     void deleteByMaterial_Id(Long materialId);
 
-    void deleteByMaterial_IdAndAuthor_Id(Long materialId, Long authorId);
 }
