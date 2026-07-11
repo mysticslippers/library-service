9d6fd9ede798d0f9b6a6248821c8394433bfe0e8
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 12:00:58 +0300

    feat: update constraint UpdateFineTariffRequest

--- backend/src/main/java/me/ifmo/backend/dto/fine/request/UpdateFineTariffRequest.java
+++ backend/src/main/java/me/ifmo/backend/dto/fine/request/UpdateFineTariffRequest.java
@@ -6,13 +6,13 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record UpdateFineTariffRequest(
-        @DecimalMin("0.00")
+        @DecimalMin("0.01")
         BigDecimal amountPerDay,
 
-        @DecimalMin("0.00")
+        @DecimalMin("0.01")
         BigDecimal fixedAmount,
 
-        @DecimalMin("0.00")
+        @DecimalMin("0.01")
         BigDecimal maxAmount,
 
         LocalDateTime validTo
