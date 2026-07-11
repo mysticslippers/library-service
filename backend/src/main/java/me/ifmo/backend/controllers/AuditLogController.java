54066319cd3169d0f9d9886a537f9fcff979451a
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 12:55:13 +0300

    feat: add @PreAuthorize("hasRole('ADMIN')") AuditLogController

--- backend/src/main/java/me/ifmo/backend/controllers/AuditLogController.java
+++ backend/src/main/java/me/ifmo/backend/controllers/AuditLogController.java
@@ -7,11 +7,13 @@ import me.ifmo.backend.dto.audit.response.AuditLogResponse;
 import me.ifmo.backend.dto.common.response.PageResponse;
 import me.ifmo.backend.services.AuditLogService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/audit-logs")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AuditLogController {
 
     private final AuditLogService service;
