b570d20016b24386742d01fa841053f9c5094a0e
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 13:03:35 +0300

    feat: delete unused methods MaterialCopyRepository

--- backend/src/main/java/me/ifmo/backend/repositories/MaterialCopyRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/MaterialCopyRepository.java
@@ -38,7 +38,5 @@ public interface MaterialCopyRepository extends JpaRepository<MaterialCopy, Long
 
     boolean existsByBranch_IdAndStatusNot(Long branchId, CopyStatus status);
 
-    List<MaterialCopy> findByMaterial_IdAndBranch_IdAndStatus(Long materialId, Long branchId, CopyStatus status);
-
     Optional<MaterialCopy> findFirstByMaterial_IdAndBranch_IdAndStatusOrderByCreatedAtAsc(Long materialId, Long branchId, CopyStatus status);
 }
