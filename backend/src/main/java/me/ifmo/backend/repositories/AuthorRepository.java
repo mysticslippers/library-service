51b91d99d4dffdee8d9a93597ec2b9b5ce3f4c16
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 13:00:33 +0300

    feat: delete unused methods AuthorRepository

--- backend/src/main/java/me/ifmo/backend/repositories/AuthorRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/AuthorRepository.java
@@ -12,25 +12,23 @@ import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    Page<Author> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
-
     @Query("""
            SELECT author FROM Author author
                WHERE lower(author.firstName) = lower(:firstName)
-                   AND lower(coalesce(author.middleName, '')) = lower(coalesce(:middleName, '')) 
-                   AND lower(author.lastName) = lower(:lastName) 
-    """)
+                   AND lower(coalesce(author.middleName, '')) = lower(coalesce(:middleName, ''))\s
+                   AND lower(author.lastName) = lower(:lastName)\s
+   \s""")
     Optional<Author> findByFullName(@Param("firstName") String firstName,
                                     @Param("middleName") String middleName,
                                     @Param("lastName") String lastName);
 
     @Query("""
-           SELECT CASE WHEN count(author) > 0 THEN TRUE ELSE FALSE END 
+           SELECT CASE WHEN count(author) > 0 THEN TRUE ELSE FALSE END\s
                FROM Author author
                WHERE lower(author.firstName) = lower(:firstName)
-                   AND lower(coalesce(author.middleName, '')) = lower(coalesce(:middleName, '')) 
-                   AND lower(author.lastName) = lower(:lastName) 
-    """)
+                   AND lower(coalesce(author.middleName, '')) = lower(coalesce(:middleName, ''))\s
+                   AND lower(author.lastName) = lower(:lastName)\s
+   \s""")
     boolean existsByFullName(@Param("firstName") String firstName,
                              @Param("middleName") String middleName,
                              @Param("lastName") String lastName);
@@ -38,10 +36,10 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     @Query("""
            SELECT author FROM Author author
                WHERE (:query IS NULL OR :query = ''
-                   OR lower(author.firstName) LIKE lower(concat('%', :query, '%')) 
-                   OR lower(coalesce(author.middleName, '')) LIKE lower(concat('%', :query, '%')) 
+                   OR lower(author.firstName) LIKE lower(concat('%', :query, '%'))\s
+                   OR lower(coalesce(author.middleName, '')) LIKE lower(concat('%', :query, '%'))\s
                    OR lower(author.lastName) LIKE lower(concat('%', :query, '%')))
                    AND author.status = :status
-    """)
+   \s""")
     Page<Author> search(@Param("query") String query, @Param("status") AuthorStatus status, Pageable pageable);
 }
