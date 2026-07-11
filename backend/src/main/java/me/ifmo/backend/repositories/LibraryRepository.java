a48e6d0f5544c193fc44a592e1ee7e05d88e32b2
misticslippers <eternal.martyr13579@gmail.com>
2026-07-11 12:31:53 +0300

    feat: add search method LibraryRepository

--- backend/src/main/java/me/ifmo/backend/repositories/LibraryRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/LibraryRepository.java
@@ -5,6 +5,8 @@ import me.ifmo.backend.entities.enums.LibraryStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -19,4 +21,17 @@ public interface LibraryRepository extends JpaRepository<Library, Long> {
     Page<Library> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
     Page<Library> findByNameContainingIgnoreCaseAndStatus(String name, LibraryStatus status, Pageable pageable);
+
+    @Query("""
+        SELECT library FROM Library library
+            WHERE (:status IS NULL OR library.status = :status)
+              AND (
+                  :query IS NULL
+                  OR LOWER(library.name) LIKE LOWER(CONCAT('%', :query, '%'))
+                  OR LOWER(library.code) LIKE LOWER(CONCAT('%', :query, '%'))
+              )
+    """)
+    Page<Library> search(@Param("query") String query,
+                         @Param("status") LibraryStatus status,
+                         Pageable pageable);
 }
