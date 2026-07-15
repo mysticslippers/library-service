22dfac9944532d3536ede224eb534e90b2fe2c22
misticslippers <eternal.martyr13579@gmail.com>
2026-07-15 15:48:30 +0300

    feat: fix bug LoanRepository

--- backend/src/main/java/me/ifmo/backend/repositories/LoanRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/LoanRepository.java
@@ -4,15 +4,13 @@ import me.ifmo.backend.entities.Loan;
 import me.ifmo.backend.entities.enums.LoanStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
 
-public interface LoanRepository extends JpaRepository<Loan, Long> {
+public interface LoanRepository extends JpaRepository<Loan, Long>, JpaSpecificationExecutor<Loan> {
 
     Page<Loan> findByUser_Id(Long userId, Pageable pageable);
 
@@ -30,28 +28,4 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     Long countByUser_IdAndStatusIn(Long userId, Collection<LoanStatus> statuses);
 
-    @Query("""
-       SELECT loan FROM Loan loan
-           WHERE (:userId IS NULL OR loan.user.id = :userId)
-             AND (:copyId IS NULL OR loan.copy.id = :copyId)
-             AND (:branchId IS NULL OR loan.branch.id = :branchId)
-             AND (:issuedByUserId IS NULL OR loan.issuedByUser.id = :issuedByUserId)
-             AND (:status IS NULL OR loan.status = :status)
-             AND (:loanedFrom IS NULL OR loan.loanedAt >= :loanedFrom)
-             AND (:loanedTo IS NULL OR loan.loanedAt <= :loanedTo)
-             AND (:dueBefore IS NULL OR loan.dueAt <= :dueBefore)
-             AND (:returnedFrom IS NULL OR loan.returnedAt >= :returnedFrom)
-             AND (:returnedTo IS NULL OR loan.returnedAt <= :returnedTo)
-    """)
-    Page<Loan> search(@Param("userId") Long userId,
-                      @Param("copyId") Long copyId,
-                      @Param("branchId") Long branchId,
-                      @Param("issuedByUserId") Long issuedByUserId,
-                      @Param("status") LoanStatus status,
-                      @Param("loanedFrom") LocalDateTime loanedFrom,
-                      @Param("loanedTo") LocalDateTime loanedTo,
-                      @Param("dueBefore") LocalDateTime dueBefore,
-                      @Param("returnedFrom") LocalDateTime returnedFrom,
-                      @Param("returnedTo") LocalDateTime returnedTo,
-                      Pageable pageable);
 }
