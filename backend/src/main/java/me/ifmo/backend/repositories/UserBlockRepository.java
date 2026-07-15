2dd0d8b1147a6ce34468e2fa730c882b78a45465
misticslippers <eternal.martyr13579@gmail.com>
2026-07-15 17:26:52 +0300

    feat: update UserBlockRepository

--- backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/UserBlockRepository.java
@@ -5,12 +5,11 @@ import me.ifmo.backend.entities.enums.UserBlockStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserBlockRepository extends JpaRepository<UserBlock, Long> {
+public interface UserBlockRepository extends JpaRepository<UserBlock, Long>, JpaSpecificationExecutor<UserBlock> {
 
     Page<UserBlock> findByUser_Id(Long userId, Pageable pageable);
 
@@ -20,14 +19,4 @@ public interface UserBlockRepository extends JpaRepository<UserBlock, Long> {
 
     Page<UserBlock> findByStatus(UserBlockStatus status, Pageable pageable);
 
-    @Query("""
-       SELECT userBlock FROM UserBlock userBlock
-           WHERE (:userId IS NULL OR userBlock.user.id = :userId)
-             AND (:createdByUserId IS NULL OR userBlock.createdByUser.id = :createdByUserId)
-             AND (:status IS NULL OR userBlock.status = :status)
-    """)
-    Page<UserBlock> search(@Param("userId") Long userId,
-                           @Param("createdByUserId") Long createdByUserId,
-                           @Param("status") UserBlockStatus status,
-                           Pageable pageable);
 }
