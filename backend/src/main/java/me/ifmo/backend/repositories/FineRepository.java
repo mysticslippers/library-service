d6ebd8b2c29e366cf8c46e5ca681f154ff8a3538
misticslippers <eternal.martyr13579@gmail.com>
2026-07-15 16:11:22 +0300

    feat: update FineRepository

--- backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/FineRepository.java
@@ -5,14 +5,12 @@ import me.ifmo.backend.entities.enums.FineStatus;
 import me.ifmo.backend.entities.enums.ViolationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface FineRepository extends JpaRepository<Fine, Long> {
+public interface FineRepository extends JpaRepository<Fine, Long>, JpaSpecificationExecutor<Fine> {
 
     Page<Fine> findByUser_Id(Long userId, Pageable pageable);
 
@@ -22,22 +20,4 @@ public interface FineRepository extends JpaRepository<Fine, Long> {
 
     Long countByUser_IdAndStatus(Long userId, FineStatus status);
 
-    @Query("""
-       SELECT fine FROM Fine fine
-           WHERE (:userId IS NULL OR fine.user.id = :userId)
-             AND (:loanId IS NULL OR fine.loan.id = :loanId)
-             AND (:copyId IS NULL OR fine.copy.id = :copyId)
-             AND (:reason IS NULL OR fine.reason = :reason)
-             AND (:status IS NULL OR fine.status = :status)
-             AND (:createdFrom IS NULL OR fine.createdAt >= :createdFrom)
-             AND (:createdTo IS NULL OR fine.createdAt <= :createdTo)
-    """)
-    Page<Fine> search(@Param("userId") Long userId,
-                      @Param("loanId") Long loanId,
-                      @Param("copyId") Long copyId,
-                      @Param("reason") ViolationType reason,
-                      @Param("status") FineStatus status,
-                      @Param("createdFrom") LocalDateTime createdFrom,
-                      @Param("createdTo") LocalDateTime createdTo,
-                      Pageable pageable);
 }
