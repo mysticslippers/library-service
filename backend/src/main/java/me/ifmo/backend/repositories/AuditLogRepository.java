6a3d041801197477a78b5158d6febd9afdd230ea
misticslippers <eternal.martyr13579@gmail.com>
2026-07-15 17:26:39 +0300

    feat: update AuditLogRepository

--- backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
+++ backend/src/main/java/me/ifmo/backend/repositories/AuditLogRepository.java
@@ -1,35 +1,12 @@
 package me.ifmo.backend.repositories;
 
 import me.ifmo.backend.entities.AuditLog;
-import me.ifmo.backend.entities.enums.AuditAction;
-import me.ifmo.backend.entities.enums.AuditEntityType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.time.LocalDateTime;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long>, JpaSpecificationExecutor<AuditLog> {
 
     Page<AuditLog> findByUser_Id(Long userId, Pageable pageable);
-
-    @Query("""
-       SELECT auditLog FROM AuditLog auditLog
-           WHERE (:actorUserId IS NULL OR auditLog.user.id = :actorUserId)
-             AND (:type IS NULL OR auditLog.type = :type)
-             AND (:entityId IS NULL OR auditLog.entityId = :entityId)
-             AND (:action IS NULL OR auditLog.action = :action)
-             AND (:createdFrom IS NULL OR auditLog.createdAt >= :createdFrom)
-             AND (:createdTo IS NULL OR auditLog.createdAt <= :createdTo)
-           ORDER BY auditLog.createdAt DESC
-    """)
-    Page<AuditLog> search(@Param("actorUserId") Long actorUserId,
-                          @Param("type") AuditEntityType type,
-                          @Param("entityId") Long entityId,
-                          @Param("action") AuditAction action,
-                          @Param("createdFrom") LocalDateTime createdFrom,
-                          @Param("createdTo") LocalDateTime createdTo,
-                          Pageable pageable);
 }
