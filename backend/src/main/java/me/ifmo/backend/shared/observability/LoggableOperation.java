44dc2fe451af03bb9ba0da1f115574418d33da65
misticslippers <eternal.martyr13579@gmail.com>
2026-07-18 13:03:15 +0300

    feat: add LoggableOperation

---
+++ backend/src/main/java/me/ifmo/backend/shared/observability/LoggableOperation.java
@@ -0,0 +1,14 @@
+package me.ifmo.backend.shared.observability;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoggableOperation {
+    String value();
+}
